cf1428573038c6b904fb4ffc674622621d3fd876
WAQAS-PC\Muhammad Waqas <aemwaqas60@gmail.com>
2018-06-14 17:13:27 +0500

    aws server url

--- public/src/environments/environment.prod.ts
+++ public/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   //  BASE_API_URL: 'http://localhost:3000/api'
-  // BASE_API_URL: 'http://13.126.127.51/api'
-  BASE_API_URL: 'https://octhub.com/api'
+   BASE_API_URL: 'https://13.126.127.51/api'
+  // BASE_API_URL: 'https://octhub.com/api'
 };
