c78ed4163fd9f4337aea2f382460cb85619a7738
rossijonas <rossijonas@gmail.com>
2018-06-25 17:04:45 -0300

    test API connection, send a basic email

--- playground/sendgrid-example.js
+++ playground/sendgrid-example.js
@@ -1,56 +1,57 @@
-var http = require("https");
+const http = require("https");
 
-var options = {
+const options = {
 	"method": "POST",
 	"hostname": "api.sendgrid.com",
 	"port": null,
 	"path": "/v3/mail/send",
 	"headers": {
-		"authorization": `Bearer ${process.env.SENDGRID_API_KEY}`,
+		"authorization": "Bearer " + process.env.SENDGRID_KEY,
 		"content-type": "application/json"
 	}
 };
 
-var req = http.request(options, function (res) {
-	var chunks = [];
+const req = http.request(options, function (res) {
+	const chunks = [];
 
 	res.on("data", function (chunk) {
 		chunks.push(chunk);
 	});
 
 	res.on("end", function () {
-		var body = Buffer.concat(chunks);
+		const body = Buffer.concat(chunks);
 		console.log(body.toString());
 	});
 });
 
-module.exports = {
-	send() {
-		req.write(JSON.stringify(
-			{ 
-				personalizations: [
-					{
-						to: [ 
-							{ 
-								email: 'octonius-test@pm.me',
-								name: 'Octonius User' 
-							} 
-						],
-						subject: 'Confirmation Mail, Testing!' 
+req.write(JSON.stringify(
+	{ 
+		personalizations: [
+			{
+				to: [ 
+					{ 
+						email: 'octonius-test@pm.me',
+						name: 'Octonius User' 
 					} 
 				],
-				from: 
-				{
-					email: 'octonius@example.com',
-					name: 'Octonius Admin'
-				},
-				reply_to: {
-					email: 'octonius@example.com', name: 'Cosmin The Boss'
-				}
+				subject: 'Confirmation Mail, Testing!' 
+			} 
+		],
+		from: 
+		{
+			email: 'octonius@example.com',
+			name: 'Octonius Admin'
+		},
+		reply_to: {
+			email: 'octonius@example.com', name: 'Cosmin The Boss'
+		},
+		content: [
+			{ 
+				type: 'text/plain',
+				value: 'It is Working'
 			}
-		));
-
-		req.end();
+		]
 	}
-}
+));
 
+req.end();
