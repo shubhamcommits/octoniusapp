4525b03c9b595370e71120e29274a45482f33de1
rossijonas <rossijonas@gmail.com>
2018-06-25 15:59:05 -0300

    include sendgrid api key on node env variables

--- nodemon_config.js
+++ nodemon_config.js
@@ -2,3 +2,4 @@ process.env.PORT = "3000";
 process.env.host = `http://localhost:${process.env.PORT}/`;
 process.env.dbURL = 'mongodb://127.0.0.1:27017/octonius';
 process.env.JWT_KEY = 'asfsaf12safas23fsafa12sf';
+process.env.SENDGRID_KEY = 'e27e13d4-24ad-4486-bacb-4037d36d53c9';
