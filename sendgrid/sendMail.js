b35908d5f7cb1a122447ec7f0acf300735233eab
rossijonas <rossijonas@gmail.com>
2018-07-03 14:58:18 -0300

    refactor sendMail to full async/await, catch errors inside the fun, no need to chain .catch on func call

--- sendgrid/sendMail.js
+++ sendgrid/sendMail.js
@@ -4,6 +4,11 @@
  * 	- create functions for each template model
  *	- provide support for the other templates
  *	- refactor
+ *
+ * 	- test it from the form too, call the function when 
+ * 	new user signup
+ * 	- test it from the form too, call the function when 
+ * 	new user signup
  */
 
 const ejs 		 = require('ejs');
@@ -14,65 +19,68 @@ const subjects = require('./templates/subjects');
 // -- MAIN FUNCTION --
 const sendMail = async (templateName, toEmail, toName, fromEmail, fromName, replyEmail, replyName) => {
 	console.log(`Sending transactional email to ${toName}...`);
-	
-	// Excratc subject, based on templateName
-	const subject = subjects[templateName];
-	
-	// Pass user data to the template
-	const templateStr = await fs.readFileSync(`./sendgrid/templates/${templateName}.ejs`);
-	const templateHtml = await ejs.render(templateStr.toString(), {name: toName});
 
-	// Sendgrid API settings
-	const config = {
-		url: '/v3/mail/send',
-		method: 'post',
-		baseURL: 'https://api.sendgrid.com',
-		'port': null,
-		transformRequest: [function(data) {
-			strData = JSON.stringify(data);
-			return strData;
-		}],
-		headers: {
-			'authorization': 'Bearer ' + process.env.SENDGRID_KEY,
-			'content-type': 'application/json'
-		},
-		data: { 
-			personalizations: [
+	try {
+		// Excratc subject, based on templateName
+		const subject = subjects[templateName];
+
+		// Pass user data to the template
+		const templateStr = await fs.readFileSync(`./sendgrid/templates/${templateName}.ejs`);
+		const templateHtml = await ejs.render(templateStr.toString(), {name: toName});
+
+		// Sendgrid API settings
+		const config = {
+			url: '/v3/mail/send',
+			method: 'post',
+			baseURL: 'https://api.sendgrid.com',
+			'port': null,
+			transformRequest: [function(data) {
+				strData = JSON.stringify(data);
+				return strData;
+			}],
+			headers: {
+				'authorization': 'Bearer ' + process.env.SENDGRID_KEY,
+				'content-type': 'application/json'
+			},
+			data: { 
+				personalizations: [
+					{
+						to: [ 
+							{ 
+								email: toEmail,
+								name: toName
+							} 
+						],
+						subject: subject 
+					} 
+				],
+				from: 
 				{
-					to: [ 
-						{ 
-							email: toEmail,
-							name: toName
-						} 
-					],
-					subject: subject 
-				} 
-			],
-			from: 
-			{
-				email: fromEmail,
-				name: fromName 
-			},
-			reply_to: {
-				email: replyEmail,
-				name: replyName
-			},
-			content: [
-				{ 
-					type: 'text/html',
-					value: templateHtml
-				}
-			]
-		}
-	};
+					email: fromEmail,
+					name: fromName 
+				},
+				reply_to: {
+					email: replyEmail,
+					name: replyName
+				},
+				content: [
+					{ 
+						type: 'text/html',
+						value: templateHtml
+					}
+				]
+			}
+		};
 
-	// Fire the request to sendgrid server, check the reponse
-	const res = await http.request(config);
+		// Fire the request to sendgrid server, check the reponse
+		const res = await http.request(config);
 
-	if (res.status === 202) {
-		return `Email sent to ${toName}`;
-	} else {
-		throw new Error();
+		if (res.status === 202) {
+			return `Email sent to ${toName}`;
+		} 
+
+	} catch (err) {
+		console.log(err);
 	}
 };
 
