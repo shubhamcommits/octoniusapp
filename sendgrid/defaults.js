3268fa58d471f831435199e10c3a39b10add6dec
rossijonas <rossijonas@pm.me>
2018-08-10 14:04:40 -0300

    refactoring sendMail, in progress

--- sendgrid/defaults.js
+++ sendgrid/defaults.js
@@ -1,9 +1,14 @@
+const generateLink = (linkType) => {
+	return process.env.NODE_ENV === 'development' ?
+	`http://localhost:3000/#/${linkType}` :
+	`https://octhub.com/#/${linkType}`;
+};
+
 module.exports = {
 	fromEmail: 'dev@octonius.com',
 	fromName: 'Team Octonius',
 	replyToEmail: 'support@octonius.com',
 	replyToMail: 'Support',
-	signupLink: process.env.NODE_ENV === 'development' ?
-	'http://localhost:3000/#/signup' :
-	'https://octhub.com/#/signup'
+	signupLink: generateLink('signup'),
+	signinLink: generateLink('signin')
 };
