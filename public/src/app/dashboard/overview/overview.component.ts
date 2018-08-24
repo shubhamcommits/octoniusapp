d1b86636f1c8f1ad13942d84d423cfaf7c47a86f
shubham3597 <vshubham5636@gmail.com>
2018-08-24 15:09:43 +0530

    checking the behaviour of posts

--- public/src/app/dashboard/overview/overview.component.ts
+++ public/src/app/dashboard/overview/overview.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { UserService } from '../../shared/services/user.service';
 import { User } from '../../shared/models/user.model';
 import { NgxUiLoaderService } from 'ngx-ui-loader'; 
+import { PostService } from '../../shared/services/post.service';
 
 @Component({
   selector: 'app-overview',
@@ -12,7 +13,13 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor(private _userService: UserService, private _authService: AuthService, private _router: Router,  private ngxService: NgxUiLoaderService) { }
+  posts = new Array();
+
+  user_data;
+  user: User;
+
+  constructor(private _userService: UserService, private _authService: AuthService, private _router: Router,  private ngxService: NgxUiLoaderService,
+  private _postservice: PostService) { }
 
   ngOnInit() {
     this.ngxService.start(); // start foreground loading with 'default' id
@@ -21,6 +28,22 @@ export class OverviewComponent implements OnInit {
     setTimeout(() => {
       this.ngxService.stop(); // stop foreground loading with 'default' id
     }, 500);
+    this.user_data = JSON.parse(localStorage.getItem('user'));
+    this.getRecentPosts();
+
+  }
+
+  getRecentPosts(){
+    this._postservice.useroverviewposts(this.user_data.user_id)
+    .subscribe((res) => {
+      // console.log('Group posts:', res);
+      this.posts = res['posts'];
+     console.log('User Post:', this.posts);
+
+
+    }, (err) => {
+
+    });
 
   }
 
