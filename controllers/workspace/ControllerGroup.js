19677440754523e271b4e132fc11215caaaf6a5c
rossijonas <rossijonas@pm.me>
2018-08-28 10:05:49 -0300

    saving progress, work interrupted to tackle other feature

--- controllers/workspace/ControllerGroup.js
+++ controllers/workspace/ControllerGroup.js
@@ -1,137 +1,107 @@
 const Group = require('../../models/group');
 const User = require('../../models/user');
+const sendMail = require('../../sendgrid/sendMail');
+const sendErr = require('../../helpers/sendErr');
 
-module.exports = {
-	/*     searchGroupUsers(req, res, next) {
-						const key_word = req.params.key_word;
-						const group_id = req.params.group_id;
+/*	===================
+ *	-- GROUP METHODS --
+ *	===================
+ */ 
 
-						Group.findOne({
-										_id: group_id
-								})
-								.populate('_members _admins', 'first_name last_name')
-								.then((group) => {
-										res.status(200).json({
-												message: "search successfull!",
-												users: group._members.concat(group._admins)
-										});
-								})
-								.catch((err) => {
-										res.status(500).json({
-												message: "Error! something went wrong | internal server error",
-												err
-										});
-								})
-
-				},
-				*/
-	// temp method for group user's searching 
-	searchGroupUsers(req, res, next) {
+const searchGroupUsers = async (req,res, next) => {
+	try {
 		const query = req.params.query;
 		const group = req.params.group_id;
 
-		User.find({
+		const users = await User.find({
 			_groups: group,
 			full_name: {
 				$regex: new RegExp(query, 'i')
 			}
-		})
+		});
 
-			.then((users) => {
+		return res.status(200).json({
+			message: `Found ${users.length} users!`,
+			users
+		});
 
-				// const members = group._admins;
-				res.status(200).json({
-					message: "users found successfully!",
-					users: users
-				});
+	} catch (err) {
+		return sendErr(res, err);
+	}
+};
 
-			})
-			.catch((err) => {
-				res.status(500).json({
-					message: "Error! something went wrong | internal server error",
-					err
-				});
-			})
+const getUserGroup = async (req, res, next) => {
+	try {
+		const groupId = req.params.group_id;
 
-	},
-
-	getUserGroup(req, res, next) {
-
-		const group_id = req.params.group_id;
-
-		Group.findOne({
-			_id: group_id
+		const group = await Group.findOne({
+			_id: groupId
 		})
 			.populate('_members', 'first_name last_name profile_pic role email')
 			.populate('_admins', 'first_name last_name profile_pic role email')
-			.then((group) => {
-				if (group == null) {
-					res.status(404).json({
-						message: "Error! group not found,Invalid group id"
-					});
-				} else {
-					res.status(200).json({
-						message: "group found successfully!",
-						group: group
-					});
+
+		if (!group) {
+			return sendErr(res, err, 'Group not found, invalid group id!', 404)
+		}
+
+		return res.status(200).json({
+			message: 'Group found!',
+			group
+		});
+	} catch (err) {
+		return sendErr(res, err);
+	}
+};
+
+// STOPPED HERE!!!!!!!!
+addNewUsersInGroup(req, res, next) {
+	let group = req.body.group;
+	let members = req.body.members;
+	let _members = members.map(result => {
+		return result._id;
+	});
+
+	Group.findByIdAndUpdate({
+		_id: group
+	}, {
+		$addToSet: {
+			_members: _members
+		}
+	}, {
+		new: true
+	})
+		.then((updated_group) => {
+			User.updateMany({
+				_id: _members
+			}, {
+				$addToSet: {
+					_groups: group
 				}
 			})
-			.catch((err) => {
-				res.status(500).json({
-					message: "Error! something went wrong | internal server error",
-					err
-				});
-			});
-	},
+				.then((updated_users) => {
+					res.status(200).json({
+						message: 'Group Data has updated successfully!',
+						group: updated_group
+					});
 
-	addNewUsersInGroup(req, res, next) {
-		let group = req.body.group;
-		let members = req.body.members;
-		let _members = members.map(result => {
-			return result._id;
-		});
-
-		Group.findByIdAndUpdate({
-			_id: group
-		}, {
-			$addToSet: {
-				_members: _members
-			}
-		}, {
-			new: true
-		})
-			.then((updated_group) => {
-				User.updateMany({
-					_id: _members
-				}, {
-					$addToSet: {
-						_groups: group
-					}
 				})
-					.then((updated_users) => {
-						res.status(200).json({
-							message: "Group Data has updated successfully!",
-							group: updated_group
-						});
-
-					})
-					.catch((err) => {
-						res.status(500).json({
-							message: "Error! something went wrong | internal server error",
-							err
-						});
-					})
-			})
-			.catch((err) => {
-				res.status(500).json({
-					message: "Error! something went wrong | internal server error",
-					err
-				});
-			})
-	},
+				.catch((err) => {
+					res.status(500).json({
+						message: 'Error! something went wrong | internal server error',
+						err
+					});
+				})
+		})
+		.catch((err) => {
+			res.status(500).json({
+				message: 'Error! something went wrong | internal server error',
+				err
+			});
+		})
+},
 
 	updateGroup(req, res, next) {
-		console.log("===============Update Group===============");
+		console.log('===============Update Group===============');
 		let group_id = req.params.group_id;
 		let data = req.body;
 
@@ -146,11 +116,11 @@ module.exports = {
 
 				if (updated_group == null) {
 					res.status(404).json({
-						message: "Error! group not found,Invalid group id"
+						message: 'Error! group not found,Invalid group id'
 					});
 				} else {
 					res.status(200).json({
-						message: "Group has been updated successfully!",
+						message: 'Group has been updated successfully!',
 						group: updated_group
 					});
 				}
@@ -159,7 +129,7 @@ module.exports = {
 			.catch((err) => {
 
 				res.status(500).json({
-					message: "something went wrong | internal server error!",
+					message: 'something went wrong | internal server error!',
 					err: err
 				});
 			})
@@ -181,7 +151,7 @@ module.exports = {
 
 				if (updated_group == null) {
 					res.status(404).json({
-						message: "Group not found!"
+						message: 'Group not found!'
 					});
 				} else {
 					res.status(200).json({
@@ -192,7 +162,7 @@ module.exports = {
 			})
 			.catch((err) => {
 				res.status(500).json({
-					message: "Something went wrong | internal server error!",
+					message: 'Something went wrong | internal server error!',
 					err: err
 				});
 			})
