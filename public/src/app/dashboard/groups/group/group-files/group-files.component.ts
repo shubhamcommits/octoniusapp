16f64599aa79fb3cf79803c8655c8485fb4d0c89
shubham3597 <vshubham5636@gmail.com>
2018-08-29 13:35:11 +0530

    files and attachment page added

--- public/src/app/dashboard/groups/group/group-files/group-files.component.ts
+++ public/src/app/dashboard/groups/group/group-files/group-files.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxUiLoaderService } from 'ngx-ui-loader'; 
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { PostService } from '../../../../shared/services/post.service'; 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { GroupService } from '../../../../shared/services/group.service';
+import { GroupDataService } from '../../../../shared/services/group-data.service';
+import { environment } from '../../../../../environments/environment';
+import { User } from '../../../../shared/models/user.model';
+import { UserService } from '../../../../shared/services/user.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-group-files',
@@ -8,7 +16,18 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 })
 export class GroupFilesComponent implements OnInit {
 
-  constructor(private ngxService: NgxUiLoaderService) { }
+  group_id;
+  groupImageUrl = '';
+  posts = new Array();
+
+  group = {
+    description: ''
+  };
+
+  isLoading$ = new BehaviorSubject(false);
+
+  constructor(private ngxService: NgxUiLoaderService, private postService: PostService,
+    public groupDataService: GroupDataService, private groupService: GroupService,private _userService: UserService) { }
 
   ngOnInit() {
     this.ngxService.start(); // start foreground loading with 'default' id
@@ -17,6 +36,67 @@ export class GroupFilesComponent implements OnInit {
     setTimeout(() => {
       this.ngxService.stop(); // stop foreground loading with 'default' id
     }, 500);
+
+    this.group_id = this.groupDataService.groupId;
+    this.loadGroupPosts();
+  }
+
+  loadGroupPosts() {
+
+    this.isLoading$.next(true);
+
+    this.postService.getGroupPosts(this.group_id)
+      .subscribe((res) => {
+        // console.log('Group posts:', res);
+        this.posts = res['posts'];
+       console.log('Group posts:', this.posts);
+       this.isLoading$.next(false);
+
+
+      }, (err) => {
+
+      });
+
+  }
+
+  onDownlaodFile(fileName, fileName_orignal) {
+
+    const fileData = {
+      'fileName': fileName
+    };
+    this._userService.downloadFile(fileData)
+      .subscribe((file) => {
+
+        //   console.log('Downloaded File', file);
+        saveAs(file, fileName_orignal);
+
+      }, (err) => {
+        console.log('Downloaded File err', err);
+
+      });
+  }
+
+  loadGroup() {
+    this.groupService.getGroup(this.group_id)
+      .subscribe((res) => {
+        console.log('Group: ', res);
+        if (res['group']['group_avatar'] == null) {
+          console.log('Inside if: ', this.groupImageUrl);
+
+          this.groupImageUrl = '/assets/images/group.png';
+        } else {
+          this.groupImageUrl = environment.BASE_URL + `/uploads/${res['group']['group_avatar']}`;
+
+        }
+
+        this.group.description = res['group']['description'];
+
+      }, (err) => {
+
+        console.log('err: ', err);
+
+      });
+
   }
 
 }
