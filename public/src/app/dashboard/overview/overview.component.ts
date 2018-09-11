b82e26990ff8b3eafdae9b46feef211689974708
shubham3597 <vshubham5636@gmail.com>
2018-09-11 20:03:16 +0530

    bug fix in overview

--- public/src/app/dashboard/overview/overview.component.ts
+++ public/src/app/dashboard/overview/overview.component.ts
@@ -62,6 +62,12 @@ export class OverviewComponent implements OnInit {
         if(this.posts[i].type=='event' && this.posts[i].completed == false){
           this.event_count=1;
         }
+        if(this.posts[i].type=='event' && this.posts[i].comments_count > 0){
+          this.normal_count=1;
+        }
+        if(this.posts[i].type=='task' && this.posts[i].comments_count > 0){
+          this.normal_count=1;
+        }
         if(this.posts[i].type=='normal'){
           this.normal_count=1;
         }
