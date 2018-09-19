b3340b50a1770ef99be154c0326bfc9b009d7020
rossijonas <rossijonas@pm.me>
2018-09-19 11:07:21 -0300

    add socket serverside

---
+++ src/utils/socket.js
@@ -0,0 +1,45 @@
+const socketIO = require('socket.io');
+
+const init = (server) => {
+  const io = socketIO(server);
+
+  /* =================
+   * - NOTIFICATIONS -
+   * =================
+   */
+
+  // FOR NEW POSTS ON GROUP FEED:
+  // -- When a user open a group activity page, connect user to this group's room
+  // -- For every new post, create the hook to broadcast the notification in respective room
+  // -- When a user leaves the group page, disconnect user from group room
+
+  io.on('connection', (socket) => {
+    console.log('--> User connected, New Post notifications enabled!');
+
+    // Join user on specific group room
+    socket.on('join', (room) => {
+      // generate room name
+      const roomName = `${room.workspace}_${room.group}`;
+
+      // join room
+      socket.join(roomName);
+
+      console.log(`User joined ${roomName} room!`);
+    });
+
+    // For every new post added, user emit to room, room broadcast to group
+    socket.on('newPostAdded', (room, userId) => {
+      // generate room name
+      const roomName = `${room.workspace}_${room.group}`;
+
+      // broadcast new post to group
+      io.to(roomName).broadcast('newPostOnGroup', userId);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('--> User disconnected!');
+    });
+  });
+};
+
+module.exports = { init };
