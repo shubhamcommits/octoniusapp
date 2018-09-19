91241031f2fc9baa83af2c4d32a41e7c04cffff8
rossijonas <rossijonas@pm.me>
2018-09-19 10:19:47 -0300

    fix app path on server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const app = require('./api/app');
+const app = require('./src/api/app');
 
 const port = process.env.PORT || '3000';
 const server = http.createServer(app);
