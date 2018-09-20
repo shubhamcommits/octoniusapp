4ba3373c226496d6fed4f03789443ec393243a6f
rossijonas <rossijonas@pm.me>
2018-09-20 12:48:59 -0300

    old post controller as a separate file, to help implementation

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -5,7 +5,13 @@ const posts = require('./posts.controller');
 const users = require('./users.controller');
 const workspaces = require('./workspaces.controller');
 
+// !! old Controllers, to be removed after implementation
+const postsController = require('./posts.controller.old');
+
 module.exports = {
+  // !! old Controllers, to be removed after implementation
+  postsController,
+  // Controllers
   auths,
   files,
   groups,
--- src/api/routes/posts.routes.js
+++ src/api/routes/posts.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { posts } = require('../controllers');
+const { posts, postsController } = require('../controllers');
 const { auth, authorization, postFileHandler } = require('../../utils');
 
 const router = express.Router();
@@ -13,19 +13,19 @@ router.use(auth.isLoggedIn);
 
 // POST api/posts/ - add new post
 router.post('/', postFileHandler, posts.add); // To do
-router.post('/add', postFileHandler, posts.addNewPost); // ! TO BE REMOVED
+router.post('/add', postFileHandler, postsController.addNewPost); // ! TO BE REMOVED
 
 // PUT api/posts/:postId - edit post
 // router.put('/:postId', authorization.toEditPost, posts.edit); // To do
-router.post('/edit', authorization.toEditPost, posts.editPost); // ! TO BE REMOVED
+router.post('/edit', authorization.toEditPost, postsController.editPost); // ! TO BE REMOVED
 
 // DELETE api/posts/:postId - delete post
 // router.delete('/:postId', authorization.todDeletePost, posts.delete); // To do
-router.put('/', posts.deletePost); // ! TO BE REMOVED
+router.put('/', postsController.deletePost); // ! TO BE REMOVED
 
 // POST api/posts/:postId/comment - add new comment on post
 // router.post('/:postId/comment', posts.addComment); // To do
-router.post('/addComment', posts.addCommentOnPost); // ! TO BE REMOVED
+router.post('/addComment', postsController.addCommentOnPost); // ! TO BE REMOVED
 
 // PUT api/posts/:postId/comment/:commentId - update comment
 // router.put('/:postId/comment/:commentId', posts.editComment); // To do
@@ -35,21 +35,21 @@ router.post('/addComment', posts.addCommentOnPost); // ! TO BE REMOVED
 
 // PUT api/posts/:postId/complete - mark post as complete
 // router.put('/:postId/complete', authorization.toCompletePost, posts.complete); // To do
-router.post('/complete', authorization.toCompletePost, posts.completePost); // ! TO BE REMOVED
+router.post('/complete', authorization.toCompletePost, postsController.completePost); // ! TO BE REMOVED
 
 // PUT api/posts/:postId/like/:userId - like post
 // router.put('/:postId/like/:userId', posts.like); // To do
-router.post('/like', posts.likePost); // ! TO BE REMOVED
+router.post('/like', postsController.likePost); // ! TO BE REMOVED
 
 // PUT api/posts/:postId/unlike/:userId - unlike post
 // router.put('/:postId/unlike/:userId', posts.unlike); // To do
-router.post('/unlike', posts.unlikePost); // ! TO BE REMOVED
+router.post('/unlike', postsController.unlikePost); // ! TO BE REMOVED
 
 // !!! move this ones to groups routes !!!
-router.get('/:group_id', posts.getGroupPosts); // ! TO BE REMOVED
-router.get('/next/:group_id/:last_post_id', posts.getGroupNextPosts); // ! TO BE REMOVED
+router.get('/:group_id', postsController.getGrouppostsController); // ! TO BE REMOVED
+router.get('/next/:group_id/:last_post_id', postsController.getGroupNextpostsController); // ! TO BE REMOVED
 
 // !!! move this one to groups routes !!!
-router.get('/userOverview/:user_id', posts.getUserOverview); // ! TO BE REMOVED
+router.get('/userOverview/:user_id', postsController.getUserOverview); // ! TO BE REMOVED
 
 module.exports = router;
