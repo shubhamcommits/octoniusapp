d289fd823badf08beae935ed1f4fee66f60c0de9
rossijonas <rossijonas@pm.me>
2018-09-21 15:03:32 -0300

    remove development logs and examples

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,14 +8,7 @@ const init = (server) => {
    * =================
    */
 
-  // FOR NEW POSTS ON GROUP FEED:
-  // -- When a user open a group activity page, connect user to this group's room
-  // -- For every new post, create the hook to broadcast the notification in respective room
-  // -- When a user leaves the group page, disconnect user from group room
-
   io.on('connection', (socket) => {
-    console.log('--> User connected, New Post notifications enabled!');
-
     // Join user on specific group room
     socket.on('join', (room) => {
       // generate room name
@@ -23,22 +16,19 @@ const init = (server) => {
 
       // join room
       socket.join(roomName);
-
-      console.log(`User joined ${roomName} room!`);
     });
 
-    // For every new post added, user emit to room, room broadcast to group
+    // -| NEW POST ON GROUP |-
     socket.on('newPost', (data) => {
       // generate room name
       const roomName = `${data.workspace}_${data.group}`;
 
-      console.log(data);
       // broadcast new post to group
       socket.broadcast.to(roomName).emit('newPostOnGroup', data);
     });
 
     socket.on('disconnect', () => {
-      console.log('--> User disconnected!');
+      // do nothing...
     });
   });
 };
