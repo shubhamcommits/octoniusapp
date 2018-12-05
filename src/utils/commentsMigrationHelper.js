840c84b68cd7212411a8c704d21127c068e1b35d
rossijonas <rossijonas@pm.me>
2018-12-05 18:24:22 -0200

    comments refactor ready to deploy, w migration tool

--- src/utils/commentsMigrationHelper.js
+++ src/utils/commentsMigrationHelper.js
@@ -9,13 +9,7 @@ const createComments = async () => {
     // Find all posts, sort by creation date
     const posts = await Post.find({})
       .sort('-_id')
-      .populate('_group', 'group_name')
-      .populate('_posted_by', 'first_name last_name profile_pic')
-      .populate('task._assigned_to', 'first_name last_name profile_pic')
       .lean();
-
-    // eslint-disable-next-line no-console
-    console.log(posts);
 
     // Update posts to the new comments strategy
     const postsUpdated = await posts.map(async (post) => {
@@ -27,43 +21,40 @@ const createComments = async () => {
               content: comment.content,
               _content_mentions: [],
               created_date: comment.created_date,
-              _commented_by: comment._commented_by,
-              _post: post._id
+              _commented_by: `${comment._commented_by}`,
+              _post: `${post._id}`,
             };
 
             // For each comment, create a comment document
             const newComment = await Comment.create(commentData);
 
+            const postCommentRemoved = await Post.findByIdAndUpdate({
+              _id: post._id 
+            }, {
+              $pull: { comments: { _id: comment._id } }
+            });
+
+            const postCommentIdAdded = await Post.findByIdAndUpdate({
+              _id: post._id 
+            }, {
+              $push: { comments: newComment._id }
+            });
+
             // Return the newComment ID to replace the previous comment object
-            return newComment._id
+            return `${newComment._id}`;
           } catch (e) {
             // eslint-disable-next-line no-console
             console.log(e);
           }
         });
 
-        // Update post.comments array with commentsUpdated
-        const postUpdated = await Post.findOneAndUpdate({
-          _id: post._id
-        }, {
-          $set: {
-            comments: commentsUpdated
-          }
-        }, {
-          new: true
-        })
-          .lean();
-
         // return comments array updated to each post
-        return postUpdated;
+        // return commentsUpdated;
       } catch (e) {
         // eslint-disable-next-line no-console
         console.log(e);
       }
     });
-
-    // eslint-disable-next-line no-console
-    console.log(postsUpdated);
 
     // eslint-disable-next-line no-console
     console.log('---- Migration Finished! -----');
