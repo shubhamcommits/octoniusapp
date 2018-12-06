6c8f2a471f22f8c0a810e847d43aaf93b2c1b3dc
Jonas B. R <rossijonas@pm.me>
2018-12-06 14:50:32 +0000

    Revert "Refactor comments - fix UI (pull request #158)"
    This reverts pull request #158.
    
    > * user routes - update user, update profile, user overview, get user changed in client end
    > * improve update user feature
    > * user routes - update user, update profile, user overview, get user changed in client end
    > * improve update user feature

--- public/src/app/shared/services/post.service.ts
+++ public/src/app/shared/services/post.service.ts
@@ -53,7 +53,7 @@ export class PostService {
     return this._http.post(this.BASE_API_URL + '/post/unlike', post);
   }
   useroverviewposts(user_id){
-    return this._http.get(this.BASE_API_URL + '/post/users/overview/'+ user_id);
+    return this._http.get(this.BASE_API_URL + '/post/userOverview/'+ user_id);
   }
 
   getPost(postId){
