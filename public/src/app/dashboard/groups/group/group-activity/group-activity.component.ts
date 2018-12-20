31c8c324429aca9cba46738cd4ef98d1d85019a0
Tijl Declerck <tijl.declerck7@gmail.com>
2018-12-20 08:26:43 -0600

    changed download file function

--- public/src/app/dashboard/groups/group/group-activity/group-activity.component.ts
+++ public/src/app/dashboard/groups/group/group-activity/group-activity.component.ts
@@ -922,17 +922,17 @@ export class GroupActivityComponent implements OnInit {
 
   }
 
-  onDownlaodFile(file) {
+  onDownlaodFile(fileName) {
 
     // const fileData = {
     //   'fileName': fileName
     // };
 
-    this.groupService.downloadGroupFile(this.group_id, file.original_name)
+    this.groupService.downloadGroupFile(this.group_id, fileName)
       .subscribe((file_toDownload) => {
 
         //   console.log('Downloaded File', file);
-        saveAs(file_toDownload, file.original_name);
+        saveAs(file_toDownload, fileName);
 
       }, (err) => {
         //  console.log('Downloaded File err', err);
