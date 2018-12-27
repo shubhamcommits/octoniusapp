fafe1e7edbd4cb53925ce90f85d0add27c98c8e5
rossijonas <rossijonas@pm.me>
2018-12-27 15:02:36 -0200

    create method to choose when to cache

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -12,9 +12,23 @@ client.get = util.promisify(client.get);
 // Save the default exec prototype method
 const exec = mongoose.Query.prototype.exec;
 
+// Cache method flags he query to be cached
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+
+  // Allow the method to be chainable
+  return this;
+};
+
 // Ovewrite the exec method with the caching strategy
 mongoose.Query.prototype.exec = async function () {
   try {
+    // Check useCache flag
+    if (!this.useCache) {
+      // If it's not set to use cache
+      return exec.apply(this, arguments);
+    }
+
     // Create a new object to store the key
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
