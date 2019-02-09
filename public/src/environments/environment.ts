c8f9044a3a665902c16c0e0476e5b6f8286027a3
shubham3597 <vshubham5636@gmail.com>
2019-02-09 22:05:34 +0530

    adding google cloud

--- public/src/environments/environment.ts
+++ public/src/environments/environment.ts
@@ -8,5 +8,10 @@ export const environment = {
   hmr: false,
   BASE_API_URL: 'http://localhost:3000/api',
   BASE_URL: 'http://localhost:3000',
-  pk_stripe: 'pk_test_rgLsr0HrrbMcqQr5G7Wz1zFK'
+  pk_stripe: 'pk_test_rgLsr0HrrbMcqQr5G7Wz1zFK',
+  
+  // !--GOOGLE DEVELOPER CONSOLE CREDENTIALS--! //
+  developerKey: 'AIzaSyDGM66BZhGSmBApm3PKL-xCrri-3Adb06I',
+  clientId: "971238950983-aef7kjl23994hjj9e8m5tch4a22b5dut.apps.googleusercontent.com",
+  // !--GOOGLE DEVELOPER CONSOLE CREDENTIALS--! //
 };
