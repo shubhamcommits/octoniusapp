48ec9dfbbb1e8709b7104eecec6d8579b561a499
Tijl Declerck <tijl.declerck7@gmail.com>
2019-03-08 08:37:38 -0600

    changed date type

--- src/api/models/comment.model.js
+++ src/api/models/comment.model.js
@@ -14,7 +14,7 @@ const CommentSchema = new Schema({
   }],
   created_date: {
     type: String,
-    default: moment.utc().format()
+    default: Date.now()
   },
   _commented_by: {
     type: Schema.Types.ObjectId,
