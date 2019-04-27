cef5a0be48e4a0ffd5f59a0e2e01fd466c8b16e1
Angel Totir <angel.totir@gmail.com>
2019-04-27 17:31:29 +0300

    remove debug console

--- public/src/app/shared/utils/quill.module.mark.js
+++ public/src/app/shared/utils/quill.module.mark.js
@@ -3,11 +3,9 @@ var Inline = Quill.import('blots/inline');
 
 class Mark extends Inline {
     static create(value) {
-        console.log("valueeeeeeeeeeeeeeeeee: ", value);
       let node = super.create(value);
       node.style.color = value.style.color;
       node.setAttribute('data-mark-id', value.id);
-    //   node.setAttribute('class', 'ql-mark ql-mark-' + value);
       return node;
     }
 
