33e02933651ecdeb04207ff08df0b9b6593cdc0c
rossijonas <rossijonas@pm.me>
2019-06-09 17:01:29 -0300

    fix db url for live editor server

--- api/utils/sharedb/sharedb-server.js
+++ api/utils/sharedb/sharedb-server.js
@@ -3,5 +3,5 @@ var ShareDB = require('sharedb');
 ShareDB.types.register(require('rich-text').type);
 
 module.exports = new ShareDB({
-  db: require('sharedb-mongo')(process.env.dbURL || 'mongodb://127.0.0.1:27017/octonius?auto_reconnect=true')
+  db: require('sharedb-mongo')(process.env.dbURL || 'mongodb://mongodb:27017/octonius?auto_reconnect=true')
 });
