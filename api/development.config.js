bed7dff6ffdbc6da3214f289261d380bd2b656c1
shubham3597 <vshubham5636@gmail.com>
2019-07-03 18:18:17 +0530

    fixing the pulse area

--- api/development.config.js
+++ api/development.config.js
@@ -2,12 +2,13 @@ const init = () => {
   process.env.NODE_ENV = 'development';
   process.env.PORT = '3000';
   process.env.host = `http://localhost:${process.env.PORT}/`;
-
-  //process.env.dbURL = 'mongodb://mongodb:27017/octonius';
-  process.env.dbURL = 'mongodb://127.0.0.1:27017/octonius';
+  // ** For development Mode using non dockered way, please comment the line 6 & 9 and comment out the line 7 & 10 ** //
+  // ** While Pushing back, change the environment variables back to the previous state ** //
+  process.env.dbURL = 'mongodb://mongodb:27017/octonius';
+  //process.env.dbURL = 'mongodb://127.0.0.1:27017/octonius';
   process.env.JWT_KEY = 'asfsaf12safas23fsafa12sf';
-  //process.env.REDIS_HOST = 'redis';
-  process.env.REDIS_HOST = '127.0.0.1';
+  process.env.REDIS_HOST = 'redis';
+  //process.env.REDIS_HOST = '127.0.0.1';
   process.env.REDIS_PORT = '6379';
   process.env.SENDGRID_KEY = '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';
   process.env.FILE_UPLOAD_FOLDER = `${__dirname}/uploads/`;
