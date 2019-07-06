e4085d14af2ebc618fd3892e4e529f55326b1142
yogesh_01 <yogeshpro01@gmail.com>
2019-07-06 11:06:34 -0400

    merge fix

--- client/src/app/dashboard/groups/group/group-tasks-new/group-tasks-new.component.ts
+++ client/src/app/dashboard/groups/group/group-tasks-new/group-tasks-new.component.ts
@@ -1414,8 +1414,8 @@ export class GroupTasksNewComponent implements OnInit {
   }
 
   entered(event: CdkDragStart<any[]>){  
-    console.log(event);
-    var title = event.source.dropContainer.data[0]['task']['status']; 
+    var title = event.source.dropContainer.data[0]['task']['status'];
+    console.log(title);
     for(var i=0; i<this.allColumns.length; i++){
       if(this.allColumns[i]['title'] != title){
         this.changeBg[i] = true;
