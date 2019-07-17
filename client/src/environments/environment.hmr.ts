f90de8de73d9e1d096bc0d7e6481876aeac0d575
shubham3597 <vshubham5636@gmail.com>
2019-07-17 18:07:10 +0530

    preparing branch for deployment

--- client/src/environments/environment.hmr.ts
+++ client/src/environments/environment.hmr.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     hmr: true,
-    BASE_API_URL: 'https://workplace.octonius.com/api',
-    BASE_URL: 'https://workplace.octonius.com',
+    BASE_API_URL: 'http://localhost:3000/api',
+    BASE_URL: 'http://localhost:3000',
     pk_stripe: 'pk_test_rgLsr0HrrbMcqQr5G7Wz1zFK',
     
     // !--GOOGLE DEVELOPER CONSOLE CREDENTIALS--! //
