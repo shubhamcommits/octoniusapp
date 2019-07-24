1c96410386ba316f085027a5906c1daeec07a0c0
Treasure Muchenagumbo <mindoftrey@gmail.com>
2019-07-24 15:00:22 -0400

    fixed inconsistency between local and live caches

--- client/src/app/shared/services/admin.service.ts
+++ client/src/app/shared/services/admin.service.ts
@@ -63,7 +63,11 @@ export class AdminService {
    */
   manuallyBustCache(): void {
     const cache = JSON.parse(localStorage.getItem('CACHE_STORAGE'));
-    cache['WorkspaceService#getWorkspace'] = undefined;
+    if (environment.production) {
+      cache['n#getWorkspace'] = undefined;
+    } else {
+      cache['WorkspaceService#getWorkspace'] = undefined;
+    }
     localStorage.setItem('CACHE_STORAGE', JSON.stringify(cache));
   }
 }
