ec45f9aa9cd6c15965262263e68f463ae8ecc5f4
wise <wazhong@ucdavis.edu>
2019-08-12 18:15:16 -0700

    added group-section upload to references, almost done with file section buttons need to finish delete, finished backend for file searches

--- client/src/app/dashboard/groups/group/group-files/group-files.component.ts
+++ client/src/app/dashboard/groups/group/group-files/group-files.component.ts
@@ -9,7 +9,7 @@ import { User } from '../../../../shared/models/user.model';
 import { UserService } from '../../../../shared/services/user.service';
 import { saveAs } from 'file-saver';
 import { DocumentFileService } from '../../../../shared/services/document-file.service';
-
+import { SnotifyService, SnotifyPosition, SnotifyToastConfig } from 'ng-snotify';
 
 @Component({
   selector: 'app-group-files',
@@ -23,6 +23,7 @@ export class GroupFilesComponent implements OnInit {
   groupImageUrl = '';
   posts = new Array();
   documentFiles = new Array();
+  allFiles = new Array();
 
   has_file = false;
 
@@ -37,82 +38,89 @@ export class GroupFilesComponent implements OnInit {
   constructor(private ngxService: NgxUiLoaderService, private postService: PostService,
     public groupDataService: GroupDataService, private groupService: GroupService,
     private _userService: UserService,
-    private documentFileService: DocumentFileService) { }
+    private documentFileService: DocumentFileService,
+    private snotifyService: SnotifyService) { }
 
   async ngOnInit() {
     this.ngxService.start(); // start foreground loading with 'default' id
     this.user_id = JSON.parse(localStorage.getItem('user'))['user_id']
     this.group_id = this.groupDataService.groupId;
-    this.loadDocumentFiles();
-    this.loadGroupUploadedFiles();
-    this.loadFiles()
+    // this.loadDocumentFiles();
+    this.loadGroupUploadedFiles()
     .then(()=>{
       this.ngxService.stop();
     })
     .catch((err)=>{
       console.log('Unexpected error', err);
-    })
+    });
+    // this.loadFiles()
+    // .then(()=>{
+    //   this.ngxService.stop();
+    // })
+    // .catch((err)=>{
+    //   console.log('Unexpected error', err);
+    // })
     // this.loadGroupPosts();
   }
 
-  loadFiles() {
-    return new Promise((resolve, reject)=>{
-      this.isLoading$.next(true);
+  // loadFiles() {
+  //   return new Promise((resolve, reject)=>{
+  //     this.isLoading$.next(true);
 
-      this.groupService.getGroupFiles(this.group_id)
-        .subscribe((res) => {
-          console.log('Group posts:', res);
-          this.posts = res['posts'];
-          for(let i = 0; i < this.posts.length; i++){
-            if(this.posts[i].files.length > 0){
-              this.has_file = true;
-              break;
+  //     this.groupService.getGroupFiles(this.group_id)
+  //       .subscribe((res) => {
+  //         console.log('Group posts:', res);
+  //         this.posts = res['posts'];
+  //         for(let i = 0; i < this.posts.length; i++){
+  //           if(this.posts[i].files.length > 0){
+  //             this.has_file = true;
+  //             break;
 
-            }
-          }
-        // console.log('Group posts:', this.posts);
-       //  console.log('Has File:', this.has_file);
-         this.isLoading$.next(false);
-         resolve();
+  //           }
+  //         }
+  //       // console.log('Group posts:', this.posts);
+  //      //  console.log('Has File:', this.has_file);
+  //        this.isLoading$.next(false);
+  //        resolve();
 
-        }, (err) => {
-          console.log('Error while loading files', err);
-          reject(err);
+  //       }, (err) => {
+  //         console.log('Error while loading files', err);
+  //         reject(err);
 
-        });
+  //       });
 
-    })
+  //   })
 
-  }
+  // }
 
-  loadGroupPosts() {
+  // loadGroupPosts() {
 
-    this.isLoading$.next(true);
+  //   this.isLoading$.next(true);
 
-    this.postService.getGroupPosts(this.group_id)
-      .subscribe((res) => {
-        // console.log('Group posts:', res);
-        this.posts = res['posts'];
-        for(var i = 0; i < this.posts.length; i++){
-          if(this.posts[i].files.length > 0){
-            this.has_file=true;
-            break;
+  //   this.postService.getGroupPosts(this.group_id)
+  //     .subscribe((res) => {
+  //       // console.log('Group posts:', res);
+  //       this.posts = res['posts'];
+  //       for(var i = 0; i < this.posts.length; i++){
+  //         if(this.posts[i].files.length > 0){
+  //           this.has_file=true;
+  //           break;
 
-          }
-          else{
-            this.has_file=false;
-          }
-        }
-    //   console.log('Group posts:', this.posts);
-      // console.log('Has File:', this.has_file);
-       this.isLoading$.next(false);
+  //         }
+  //         else{
+  //           this.has_file=false;
+  //         }
+  //       }
+  //   //   console.log('Group posts:', this.posts);
+  //     // console.log('Has File:', this.has_file);
+  //      this.isLoading$.next(false);
 
 
-      }, (err) => {
+  //     }, (err) => {
 
-      });
+  //     });
 
-  }
+  // }
 
   onDownlaodFile(fileName, fileName_orignal) {
 
@@ -164,22 +172,22 @@ export class GroupFilesComponent implements OnInit {
 
   }
 
-  loadDocumentFiles(){
-    return new Promise((resolve, reject)=>{
-      this.documentFileService.getFiles(this.group_id)
-      .subscribe((res)=>{
-        console.log('All document files', res);
-        if(res['file'].length > 0){
-          this.documentFiles = res['file'];
-          this.has_file = true;
-        }
-        resolve();
-      }, (err)=>{
-        console.log('Error occured while fetching the group document files', err);
-        reject(err);
-      })
-    })
-  }
+  // loadDocumentFiles(){
+  //   return new Promise((resolve, reject)=>{
+  //     this.documentFileService.getFiles(this.group_id)
+  //     .subscribe((res)=>{
+  //       console.log('All document files', res);
+  //       if(res['file'].length > 0){
+  //         this.documentFiles = res['file'];
+  //         this.has_file = true;
+  //       }
+  //       resolve();
+  //     }, (err)=>{
+  //       console.log('Error occured while fetching the group document files', err);
+  //       reject(err);
+  //     })
+  //   })
+  // }
 
   addFileEvent(fileInput:any){
     var formData = new FormData()
@@ -192,18 +200,34 @@ export class GroupFilesComponent implements OnInit {
       }
     this.groupService.addGroupFileInFileSection(this.group_id,this.user_id,formData)
     .subscribe((res) => {
-      console.log('Group posts:', res);
-      
 
+      this.allFiles.unshift(res['filesFromFileSectionNewUpload'])
+      this.snotifyService.success(`Files Uploaded!`, {
+        timeout: 1500,
+        showProgressBar: false,
+      });
     }, (err) => {
-      console.log('Error while loading files', err);
+      console.log('Error while uploading files', err);
+      this.snotifyService.error(`Error while uploading files!`, {
+        timeout: 1500,
+        showProgressBar: false,
+      });
     });
   }
+
   loadGroupUploadedFiles(){
     return new Promise((resolve, reject)=>{
+      this.isLoading$.next(true);
       this.groupService.getGroupFileInFileSection(this.group_id)
       .subscribe((res)=>{
-        console.log('All document files', res);
+        // console.log(res["concatAllFiles"])
+        if(res['concatAllFiles'].length > 0){
+          this.has_file = true
+          this.allFiles = res['concatAllFiles']
+        }else{
+          this.has_file = false
+        }
+        this.isLoading$.next(false)
         resolve();
       }, (err)=>{
         console.log('Error occured while fetching the group document files', err);
