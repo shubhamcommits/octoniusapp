af1922db962e0d89e824aedf1b67c5c3859ed80d
wise <wazhong@ucdavis.edu>
2019-09-05 10:47:03 -0700

    added in previews for activity, finished preview for file section, still need to do checks

--- client/src/app/common/components/posts/event-group-post/event-group-post.component.ts
+++ client/src/app/common/components/posts/event-group-post/event-group-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild, Renderer, ElementRef } from '@angular/core';
 import { saveAs } from 'file-saver';
 import  moment from "moment";
 import { takeUntil } from "rxjs/operators";
@@ -8,7 +8,7 @@ import { GroupService } from "../../../../shared/services/group.service";
 import { PostService } from "../../../../shared/services/post.service";
 import { SearchService } from '../../../../shared/services/search.service';
 import { CommentSectionComponent } from "../../comments/comment-section/comment-section.component";
-declare var $;
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'event-group-post',
@@ -73,13 +73,32 @@ export class EventGroupPostComponent implements OnInit, OnDestroy {
   tags_search_words: String = ''
   tags_search_result: any = new Array();
 
+  //file preview
+  listenFuncForImageClicks: Function;
+  pdfSourceLinks = ""
+  iFrameSourceLinks
+  imageSourceLinks = []
+
   // collapsibility
   // If true, "read more" text should be displayed and the post should be in preview mode
   // If false, "read less" text should be displayed and the post should be displayed entirely
   readMore: boolean;
 
   constructor(private postService: PostService, private groupService: GroupService,
-    private searchService: SearchService) { }
+    private searchService: SearchService,
+        private renderer: Renderer,
+    private elementRef: ElementRef,
+    public sanitizer: DomSanitizer,) {
+      this.listenFuncForImageClicks = this.renderer.listen(this.elementRef.nativeElement, 'click', (event) => {
+
+        if(event.target.className === "imagePreview" && event.target.tagName == "IMG"){
+            this.imageSourceLinks = [...this.imageSourceLinks, event.target.src] 
+       
+            document.body.style.overflow = "hidden"
+        }
+    });
+
+    }
 
   ngOnInit() {
     this.commentCount = this.post.comments.length;
@@ -100,13 +119,20 @@ export class EventGroupPostComponent implements OnInit, OnDestroy {
          }
 
         this.readMore = this.preview;
+
+    if(this.post['files'].length > 0){
+
+      const allCurrentIndexFiles = this.post['files'].forEach(innerPostFiles => {
+        if (innerPostFiles.orignal_name){
+          const mimeTypeFile = innerPostFiles.orignal_name.substring(innerPostFiles.orignal_name.lastIndexOf('.') + 1)
+          innerPostFiles["mimeType"] = mimeTypeFile
+
+        }else{
+          innerPostFiles["mimeType"] = "noMime"
+        }
+      });
+}
   }
-  ngAfterViewInit(): void {
-    $('.image-gallery').lightGallery({
-      share:false,
-      counter:false
-    });
- }
 
   applyZoom(htmlDOM): string{
     var parser = new DOMParser();
@@ -116,11 +142,11 @@ export class EventGroupPostComponent implements OnInit, OnDestroy {
 
     for(var _i=0; _i<imgTag.length; _i++){
       let img:any = doc.getElementsByTagName('img')[_i];
+      img.classList.add("imagePreview");
       let clonedImg:any=img.cloneNode(true);
       let acnhorThumbnail=document.createElement('a');
-      acnhorThumbnail.href=clonedImg.src;
+      acnhorThumbnail.href="javascript:void(0)";
       let imgGallery = document.createElement("div");
-      imgGallery.classList.add('image-gallery');
       acnhorThumbnail.appendChild(clonedImg);
       imgGallery.appendChild(acnhorThumbnail);
       img.replaceWith(imgGallery);
@@ -354,6 +380,7 @@ export class EventGroupPostComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
+    this.listenFuncForImageClicks();
   }
 
   tagListSearch(){
@@ -380,4 +407,48 @@ export class EventGroupPostComponent implements OnInit, OnDestroy {
     console.log(this.tags);
   }
 
+  imagePreviewClicked(src:string){
+    this.imageSourceLinks = [...this.imageSourceLinks, `${this.BASE_URL}/uploads/${src}`] 
+    document.body.style.overflow = "hidden"
+  }
+
+  pdfPreviewClicked(src:string){
+    this.pdfSourceLinks = `${this.BASE_URL}/uploads/${src}`
+    document.body.style.overflow = "hidden"
+  }
+  
+//this checks for .ppt, .pptx, .doc, .docx, .xls and .xlsx
+  officeMimeTypeclick(src:string){
+    this.iFrameSourceLinks = `${this.BASE_URL}/uploads/${src}`
+    document.body.style.overflow = "hidden"
+    this.iFrameSourceLinks = this.sanitizer.bypassSecurityTrustResourceUrl(`https://view.officeapps.live.com/op/embed.aspx?src=${this.iFrameSourceLinks}`);
+
+  }
+
+  googleMimeTypeclick(src:string){
+    this.iFrameSourceLinks = `${this.BASE_URL}/uploads/${src}`
+    document.body.style.overflow = "hidden"
+    this.iFrameSourceLinks = this.sanitizer.bypassSecurityTrustResourceUrl(`https://docs.google.com/viewer?url=${this.iFrameSourceLinks}&embedded=true`);
+  }
+
+  overlayRemoval(mimetype:String){
+    document.body.style.overflow = ""
+
+    switch (mimetype) {
+      case 'pdf':
+        this.pdfSourceLinks = ""
+        break;
+      case 'images': 
+      this.imageSourceLinks = []
+        break;
+      case 'otherFilesForiFrame':
+         document.getElementById("overlay-iframe").remove()
+         this.iFrameSourceLinks = ""  
+        break;
+
+     default:
+        event.stopPropagation();
+       break;
+   }
+  }
 }
