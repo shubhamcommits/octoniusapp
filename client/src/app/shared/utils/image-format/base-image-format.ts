98f3899d1ef4c5874c53f65ca7881c4fa7fe5426
shubham3597 <vshubham5636@gmail.com>
2019-09-19 13:28:20 +0530

    image resize completed in octodoc

---
+++ client/src/app/shared/utils/image-format/base-image-format.ts
@@ -0,0 +1,36 @@
+import Quill from 'quill';
+(window as any).Quill = Quill;
+
+var BaseImageFormat = Quill.import('formats/image');
+const ImageFormatAttributesList = [
+    'alt',
+    'height',
+    'width',
+    'style'
+];
+
+class ImageFormat extends BaseImageFormat {
+  static formats(domNode) {
+    return ImageFormatAttributesList.reduce(function(formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+  format(name, value) {
+    if (ImageFormatAttributesList.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+
+Quill.register(ImageFormat, true);
+
+export { ImageFormat }
