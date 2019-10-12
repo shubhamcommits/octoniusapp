f6ba8487a99be44f489ba4d570dcd03b50935521
cosmin ciobanu <cosmin@octonius.com>
2019-10-12 21:41:21 +0300

    added new keys stripe

--- api/development.config.js
+++ api/development.config.js
@@ -14,7 +14,7 @@ const init = () => {
   process.env.FILE_UPLOAD_FOLDER = `${__dirname}/uploads/`;
   //process.env.SK_STRIPE = 'sk_test_dvebbZQPA4Vk8kKZaEuN32sD';
   process.env.SK_STRIPE = 'sk_live_WupyMoQf93S59gwTqmYpmWel';
-  process.env.stripe_plan = 'plan_EK1uRUJLJcDS6e';
+  process.env.stripe_plan = 'prod_EK1qbKCxtCrFmt';
   process.env.WEBHOOK_PS_SECRET = 'whsec_pmcLdxoYxBAdZswT2ZzWYep2WmnBW8Sn';
 };
 
