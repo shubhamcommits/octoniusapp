7aa8bef9aad3053c86e9d9ae2343f844e973731c
Anghel Dan-Adrian <angheldanadrian@gmail.com>
2019-12-02 22:31:12 +0200

    Filter my space tasks by dueDate for today and thisWeek

--- client/src/app/dashboard/overview/overview-my-tasks/overview-my-tasks.component.ts
+++ client/src/app/dashboard/overview/overview-my-tasks/overview-my-tasks.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { environment } from '../../../../environments/environment';
-import { BehaviorSubject } from 'rxjs';
-import { PostService } from '../../../shared/services/post.service';
-import { UserService } from '../../../shared/services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {NgxUiLoaderService} from 'ngx-ui-loader';
+import {PostService} from '../../../shared/services/post.service';
+import {UserService} from '../../../shared/services/user.service';
 
 @Component({
   selector: 'app-overview-my-tasks',
@@ -14,6 +12,9 @@ export class OverviewMyTasksComponent implements OnInit {
 
   posts = [];
   recentPosts: any = [];
+
+  todayTasks: any = [];
+  thisWeekTasks: any = [];
 
   constructor(private ngxService: NgxUiLoaderService, private _postservice: PostService, private userService: UserService) {
 
@@ -26,32 +27,67 @@ export class OverviewMyTasksComponent implements OnInit {
     setTimeout(() => {
       this.ngxService.stop(); // stop foreground loading with 'default' id
     }, 500);
-    this.recentPosts = await this.getRecentPosts();
-    this.recentPosts = this.recentPosts.concat(await this.getCompletedPosts());
+
+    /*this.recentPosts = await this.getRecentPosts();
+    this.recentPosts = this.recentPosts.concat(await this.getCompletedPosts());*/
+
+    this.todayTasks = await this.getTodayTasks();
+    this.thisWeekTasks = await this.getThisWeekTasks();
+
+    console.log(this.todayTasks);
+    console.log(this.thisWeekTasks);
 
   }
 
   getRecentPosts() {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       this.userService.getUserTasks()
-      .subscribe((res) => {
-        //console.log('Group posts:', res);
-       resolve(res['posts']);
-      }, (err) => {
+        .subscribe((res) => {
+          //console.log('Group posts:', res);
+          resolve(res['posts']);
+        }, (err) => {
           reject([]);
-      });
+        });
     });
   }
 
   getCompletedPosts() {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       this.userService.getCompletedUserTasks()
-      .subscribe((res) => {
-        //console.log('Group posts:', res);
-       resolve(res['posts']);
-      }, (err) => {
+        .subscribe((res) => {
+          //console.log('Group posts:', res);
+          resolve(res['posts']);
+        }, (err) => {
           reject([]);
-      });
+        });
+    });
+  }
+
+  getTodayTasks() {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserTodayTasks()
+        .subscribe((res) => {
+
+          console.log('Today tasks:', res);
+
+          resolve(res['tasks']);
+        }, (err) => {
+          reject([]);
+        });
+    });
+  }
+
+  getThisWeekTasks() {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserThisWeekTasks()
+        .subscribe((res) => {
+
+          console.log('This week tasks:', res);
+
+          resolve(res['tasks']);
+        }, (err) => {
+          reject([]);
+        });
     });
   }
 
