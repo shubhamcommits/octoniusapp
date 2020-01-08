6a7d1beb3c6fce30e26d7aeff592044b0b2ecc38
kidex <cemaifaC!1>
2020-01-08 22:54:10 +0200

    add external key for requests without authentication

--- api/api/services/external.service.js
+++ api/api/services/external.service.js
@@ -1,9 +1,19 @@
 const Workplace = require('./../models/workspace.model');
 
+const headerExternalKey = "HEADERIDSHAREDFOROCTONIUS";
+
+
 const externalService = {
     async getAllWorkplaces(req, res) {
         try {
-            return res.status(200).json(await Workplace.find());
+            if(req.headers.externalkey === headerExternalKey) {
+                return res.status(200).json(await Workplace.find());
+            } else {
+                res.status(404).json({
+                    message: '404 not found'
+                });
+            }
+
         } catch (e) {
             res.status(500).json({message: 'Failed to get all workspaces'});
         }
