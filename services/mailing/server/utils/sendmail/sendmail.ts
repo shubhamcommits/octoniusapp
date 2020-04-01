30028175d41edb8f807da2cf5ac93a19ad7cc26f
shubhamcommits <vshubham5636@gmail.com>
2020-04-01 15:34:14 +0530

    refactoring services

--- services/mailing/server/utils/sendmail/sendmail.ts
+++ services/mailing/server/utils/sendmail/sendmail.ts
@@ -69,9 +69,13 @@ const postMailHelper = async (post: any, emailType: any, user?: any) => {
   // Generate email body from template
   const emailBody = await generateEmailBody(emailType, emailData);
 
-  // Send email to task post
-  if (post.type === 'task')
+  // Send email as a task reminder post
+  if (post.type === 'task' && emailType === 'scheduleTaskReminder')
     await sendMail(emailBody, emailData, { date: moment.utc(post.task.due_to, 'YYYY-MM-DD').startOf('day').format() })
+
+  // Send email to task post
+  else if (post.type === 'task' && (emailType === 'taskAssigned' || emailType === 'taskReassigned'))
+    await sendMail(emailBody, emailData)
 
   // Else send mail to normal post
   else if (post.type === 'normal')
@@ -120,8 +124,13 @@ const eventMailHelper = async (res: Response, post: any, emailType: any) => {
     // Generate email body from template.
     const emailBody = await generateEmailBody(emailType, emailData);
 
-    // Send email
-    await sendMail(emailBody, emailData, { date: moment.utc(post.event.due_to, 'YYYY-MM-DD').startOf('day').format() })
+    // Send email to event reminder
+    if(emailType === 'scheduleEventReminder')
+      await sendMail(emailBody, emailData, { date: moment.utc(post.event.due_to, 'YYYY-MM-DD').startOf('day').format() })
+
+    // Else only event assigned
+    else if(emailType === 'eventAssigned')
+      await sendMail(emailBody, emailData)
 
     // Send status 200 response
     return res.status(200).json({
@@ -369,6 +378,41 @@ const joinedGroup = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+
+// Join workspace invitation email
+const joinWorkspace = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const emailType = 'joinWorkspace';
+    const { reqData } = req.body;
+    // Generate email data
+    const from: any = await User.findById({ _id: reqData.user_id });
+    const workspace: any = await Workspace.findById({ _id: reqData.workspace_id });
+
+    const emailData = {
+      subject: subjects[emailType],
+      toName: '',
+      toEmail: reqData.email,
+      fromName: from.first_name,
+      fromEmail: from.email,
+      workspace: workspace.workspace_name,
+      link: defaults.signupLink(workspace.workspace_name)
+    };
+
+    // Generate email body from template
+    const emailBody = await generateEmailBody(emailType, emailData);
+
+    // Send email
+    const send = await sendMail(emailBody, emailData);
+    return res.status(200).json({
+      message: `Join Group email sent!`,
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    return sendError(res, err, 'Internal Server Error!', 500);
+  }
+};
+
+
 /**
  * =========================
  *           MENTIONS
@@ -377,49 +421,10 @@ const joinedGroup = async (req: Request, res: Response, next: NextFunction) => {
 
 /**
  * This function is responsible for sending the mail to the mentioned user
- * @param { body: { post, user } }req 
+ * @param { body: { post, user } } req 
  * @param res 
  * @param next 
  */
-// Join workspace invitation email
-const joinWorkspace = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const emailType = 'joinWorkspace';
-      const { reqData } = req.body;
-      // Generate email data
-      const from:any = await User.findById({ _id: reqData.user_id });
-      const workspace:any = await Workspace.findById({ _id: reqData.workspace_id });
-  
-      const emailData = {
-        subject: subjects[emailType],
-        toName: '',
-        toEmail: reqData.email,
-        fromName: from.first_name,
-        fromEmail: from.email,
-        workspace: workspace.workspace_name,
-        link: defaults.signupLink(workspace.workspace_name)
-      };
-  
-      // Generate email body from template
-      const emailBody = await generateEmailBody(emailType, emailData);
-  
-      // Send email
-      const send = await sendMail(emailBody, emailData);
-      return res.status(200).json({
-        message: `Join Group email sent!`,
-    });
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      return sendError(res, err, 'Internal Server Error!', 500);
-    }
-  };
-
-
-// =========================
-//          MENTIONS
-// ==========================
-
-// Send an email when a user is mentioned in a post
 const userMentionedPost = async (req: Request, res: Response, next: NextFunction) => {
 
   // Post and User Data from request
@@ -518,101 +523,88 @@ const taskAssigned = async (req: Request, res: Response, next: NextFunction) => 
 
 
 /**
+ * This function is responsible for sending email to the assigned user of task(reassigning mail)
+ * @param { body:{ post } }req 
+ * @param res 
+ * @param next 
+ */
+const taskReassigned = async (req: Request, res: Response, next: NextFunction) => {
+
+  // Post object from request
+  const { post } = req.body;
+
+  try {
+
+    // Defining Email Type for templates
+    const emailType = 'taskReassigned';
+
+    // Send Mail to task assignee
+    await postMailHelper(post, emailType);
+
+    // Send status 200 response
+    return res.status(200).json({
+      message: 'User task reassigned mail sent!'
+    })
+
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    return sendError(res, new Error(err), 'Internal Server Error!', 500);
+  }
+};
+
+
+// send an email when a task is completed
+const userCompletedTask = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { userWhoChangedStatusId, post } = req.body;
+    const emailType = 'userCompletedTask';
+
+    // Generate email data
+    const appointer: any = await User.findById({ _id: post._posted_by });
+    const appointee: any = await User.findById({ _id: post.task._assigned_to });
+    const userWhoChangedStatus: any = await User.findById({ _id: userWhoChangedStatusId });
+    const group: any = await Group.findById({ _id: post._group });
+
+    // we want to send emails to the appointer and the appointee
+    // if they are the same person then we only want to send one email
+    const destinationUsers = appointer._id === appointee._id ? [appointer] : [appointer, appointee];
+
+    destinationUsers.forEach(async (user) => {
+      const emailData = {
+        subject: subjects[emailType],
+        appointeeName: appointee.first_name,
+        appointerName: appointer.first_name,
+        userWhoChangedStatusName: userWhoChangedStatus.first_name,
+        toName: user.first_name,
+        toEmail: user.email,
+        contentTitle: post.title,
+        contentPost: post.content,
+        workspace: group.workspace_name,
+        group: group.group_name,
+        link: defaults.signinLink,
+        postLink: defaults.postLink(group._id, post._id)
+      };
+
+      // Generate email body from template
+      const emailBody = await generateEmailBody(emailType, emailData);
+
+      // Send email
+      const send = await sendMail(emailBody, emailData);
+      return res.status(200).json({
+        message: 'User Task Completed mail sent'
+      });
+    });
+  } catch (err) {
+    return sendError(res, new Error(err), 'Internal Server Error!', 500);
+  }
+};
+
+/**
  * This function is responsible for sending email to the assigned users of event
  * @param { body:{ post } }req 
  * @param res 
  * @param next 
  */
-  // Send email when a task is reassigned to a user
-const taskReassigned = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { postUpdated } = req.body;
-      const emailType = 'taskReassigned';
-  
-      // Generate email data
-      const to:any = await User.findById({ _id: postUpdated.task._assigned_to  });
-      const from:any = await User.findById({ _id: postUpdated._posted_by });
-      const group:any = await Group.findById({ _id: postUpdated._group });
-  
-      const emailData = {
-        subject: subjects[emailType],
-        toName: to.first_name,
-        toEmail: to.email,
-        fromName: from.first_name,
-        fromEmail: from.email,
-        contentTitle: postUpdated.title,
-        contentPost: postUpdated.content,
-        workspace: group.workspace_name,
-        group: group.group_name,
-        link: defaults.signinLink,
-        taskLink: defaults.postLink(group._id, postUpdated._id)
-      };
-  
-      console.log('emailData', emailData);
-  
-      // Generate email body from template
-      const emailBody = await generateEmailBody(emailType, emailData);
-  
-      // Send email
-      const send = await sendMail(emailBody, emailData);
-      return res.status(200).json({
-        message: 'Task Reassigned mail sent'
-      });
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      return sendError(res, new Error(err), 'Internal Server Error!', 500);
-    }
-  };
-
-
-  // send an email when a task is completed
-const userCompletedTask = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { userWhoChangedStatusId, post } = req.body;
-      const emailType = 'userCompletedTask';
-  
-      // Generate email data
-      const appointer:any = await User.findById({ _id: post._posted_by });
-      const appointee:any = await User.findById({ _id: post.task._assigned_to });
-      const userWhoChangedStatus:any = await User.findById({ _id: userWhoChangedStatusId });
-      const group:any = await Group.findById({ _id: post._group });
-  
-      // we want to send emails to the appointer and the appointee
-      // if they are the same person then we only want to send one email
-      const destinationUsers = appointer._id === appointee._id ? [appointer] : [appointer, appointee];
-  
-      destinationUsers.forEach(async (user) => {
-        const emailData = {
-          subject: subjects[emailType],
-          appointeeName: appointee.first_name,
-          appointerName: appointer.first_name,
-          userWhoChangedStatusName: userWhoChangedStatus.first_name,
-          toName: user.first_name,
-          toEmail: user.email,
-          contentTitle: post.title,
-          contentPost: post.content,
-          workspace: group.workspace_name,
-          group: group.group_name,
-          link: defaults.signinLink,
-          postLink: defaults.postLink(group._id, post._id)
-        };
-  
-        // Generate email body from template
-        const emailBody = await generateEmailBody(emailType, emailData);
-  
-        // Send email
-        const send = await sendMail(emailBody, emailData);
-        return res.status(200).json({
-            message: 'User Task Completed mail sent'
-          });
-      });
-    } catch (err) {
-        return sendError(res, new Error(err), 'Internal Server Error!', 500);
-    }
-  };
-
-
-  // Send email when a task assigned to a user
 const eventAssigned = async (req: Request, res: Response, next: NextFunction) => {
 
   // Post object from request
@@ -721,15 +713,15 @@ export {
   // Event Reminder
   scheduleEventReminder,
 
-    // Event Assigned
-    eventAssigned,
+  // Event Assigned
+  eventAssigned,
 
-    // Workspace Joined
-    joinWorkspace,
+  // Workspace Joined
+  joinWorkspace,
 
-    // Task Reassigned
-    taskReassigned,
+  // Task Reassigned
+  taskReassigned,
 
-    // User Task Completed
-    userCompletedTask
+  // User Task Completed
+  userCompletedTask
 }
