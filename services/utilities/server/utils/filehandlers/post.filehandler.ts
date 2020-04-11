b117e0f5776d7b6376e6b01ef8e2b735913cdb8d
Anish Goyal <anishgoyal2406@gmail.com>
2020-04-11 16:43:53 +0530

    file upload

--- services/utilities/server/utils/filehandlers/post.filehandler.ts
+++ services/utilities/server/utils/filehandlers/post.filehandler.ts
@@ -1,4 +1,5 @@
 import { Response, Request, NextFunction } from "express";
+const multer = require("multer");
 
 /**
  * This function is the boiler plate for file handler mechanism for user post attachements
@@ -8,97 +9,133 @@ import { Response, Request, NextFunction } from "express";
  */
 const postFileHandler = (req: Request, res: Response, next: NextFunction) => {
 
-  // Initialize the req['files'] object
-  let files: any = req['files']
+  // // Initialize the req['files'] object
+  // let files: any = req['files']
 
-  // Check the current request has files object underlying
-  if (!files) {
+  // // Check the current request has files object underlying
+  // if (!files) {
 
-    // Set the body.files object as null
-    req.body.files = null;
+  //   // Set the body.files object as null
+  //   req.body.files = null;
 
-    // Pass the middleware
-    next();
+  //   // Pass the middleware
+  //   next();
 
-    // If multiple files are attached with post
-  } else if (files.attachments.length > 1) {
+  //   // If multiple files are attached with post
+  // } else if (files.attachments.length > 1) {
 
-    // Set the files property to an array
-    req.body.files = [];
+  //   // Set the files property to an array
+  //   req.body.files = [];
 
-    // Fetch the files from the current request
-    files.attachments.forEach((currentFile: any, index: Number) => {
+  //   // Fetch the files from the current request
+  //   files.attachments.forEach((currentFile: any, index: Number) => {
 
-      // Instantiate the fileName variable and add the date object in the name
-      let fileName = Date.now().toString() + currentFile.name;
+  //     // Instantiate the fileName variable and add the date object in the name
+  //     let fileName = Date.now().toString() + currentFile.name;
 
-      // Get the folder link from the environment
-      const folder = process.env.FILE_UPLOAD_FOLDER;
+  //     // Get the folder link from the environment
+  //     const folder = process.env.FILE_UPLOAD_FOLDER;
 
-      // Modify the file accordingly and handle request
-      currentFile.mv(folder + fileName, (error: Error) => {
-        if (error) {
-          fileName = null;
-          return res.status(500).json({
-            status: '500',
-            message: 'file upload error',
-            error: error
-          });
-        }
-      });
+  //     // Modify the file accordingly and handle request
+  //     currentFile.mv(folder + fileName, (error: Error) => {
+  //       if (error) {
+  //         fileName = null;
+  //         return res.status(500).json({
+  //           status: '500',
+  //           message: 'file upload error',
+  //           error: error
+  //         });
+  //       }
+  //     });
 
-      // Modify the file and serialise the object
-      const file = {
-        orignal_name: currentFile.name,
-        modified_name: fileName
-      };
+  //     // Modify the file and serialise the object
+  //     const file = {
+  //       orignal_name: currentFile.name,
+  //       modified_name: fileName
+  //     };
 
-      // Push the file object
-      req.body.files.push(file);
-    });
+  //     // Push the file object
+  //     req.body.files.push(file);
+  //   });
 
-    // Pass the middleware
-    next();
+  //   // Pass the middleware
+  //   next();
 
-    // If only single file is attached with post
-  } else {
+  //   // If only single file is attached with post
+  // } else {
 
-    // Set the files property to an array
-    req.body.files = [];
+  //   // Set the files property to an array
+  //   req.body.files = [];
 
-    // Instantiate the fileName variable and add the date object in the name
-    let fileName = Date.now().toString() + req['files'].attachments['name'];
+  //   // Instantiate the fileName variable and add the date object in the name
+  //   let fileName = Date.now().toString() + req['files'].attachments['name'];
 
-    // Fetch the file from the current request
-    const currentFile: any = req['files'].attachments;
+  //   // Fetch the file from the current request
+  //   const currentFile: any = req['files'].attachments;
 
-    // Get the folder link from the environment
-    const folder = process.env.FILE_UPLOAD_FOLDER;
+  //   // Get the folder link from the environment
+  //   const folder = process.env.FILE_UPLOAD_FOLDER;
 
-    // Modify the file accordingly and handle request
-    currentFile.mv(folder + fileName, (error: Error) => {
-      if (error) {
-        fileName = null;
-        return res.status(500).json({
-          status: '500',
-          message: 'file upload error',
-          error: error
-        });
-      }
+  //   // Modify the file accordingly and handle request
+  //   currentFile.mv(folder + fileName, (error: Error) => {
+  //     if (error) {
+  //       fileName = null;
+  //       return res.status(500).json({
+  //         status: '500',
+  //         message: 'file upload error',
+  //         error: error
+  //       });
+  //     }
+  //   })
+
+  //   // Modify the file and serialise the object
+  //   const file = {
+  //     orignal_name: req['files'].attachments['name'],
+  //     modified_name: fileName
+  //   };
+
+  //   // Push the file object
+  //   req.body.files.push(file);
+
+  //   // Pass the middleware
+  //   next();
+  // }
+
+  let files: any = req['files'];
+
+  // If no files present
+  if (!files){
+    return res.status(500).json({
+      message: "0 Files selected"
     })
-
-    // Modify the file and serialise the object
-    const file = {
-      orignal_name: req['files'].attachments['name'],
-      modified_name: fileName
-    };
-
-    // Push the file object
-    req.body.files.push(file);
-
-    // Pass the middleware
-    next();
   }
+
+  const folder = process.env.FILE_UPLOAD_FOLDER;
+  console.log(folder);
+  var Storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+      callback(null, folder);
+    },
+    filename: function(req, file, callback) {
+      callback(null, file.fieldname + "_" + Date.now() + '_' + file.file.originalname);
+    }
+  });
+  var upload = multer({
+    storage: Storage
+  }).array('files', 20);
+
+  upload(req, res, function(err){
+    if (err){
+      return res.status(500).json({
+        message: err
+      })
+    }
+    else{
+      return res.status(200).json({
+        message: "success"
+      })
+    }
+  })
 }
 
 export { postFileHandler }
