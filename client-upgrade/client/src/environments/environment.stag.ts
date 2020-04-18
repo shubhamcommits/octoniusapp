3bac441e4970093a64da5eccd36a6bb2cc2ae35e
shubhamcommits <vshubham5636@gmail.com>
2020-04-18 13:34:18 +0530

    deploying the branch with sockets connection fix

--- client-upgrade/client/src/environments/environment.stag.ts
+++ client-upgrade/client/src/environments/environment.stag.ts
@@ -1,69 +1,62 @@
 export const environment = {
 
-    // Environment Configs
-    production: true,
-    hmr: false,
+  // Environment Configs
+  production: true,
+  hmr: false,
 
-    // Protocol Type
-    protocol: 'https://',
+  // Stripe key
+  pk_stripe: 'pk_live_C6kLYxOOeUXt0hzaK6PockGM',
 
-    // Domain Name
-    domain: 'localhost',
-  
-    // Base URLs
-    BASE_API_URL: 'http://localhost:3000/api',
-    BASE_URL: 'http://localhost:3000',
-  
-    // Stripe key
-    pk_stripe: 'pk_test_rgLsr0HrrbMcqQr5G7Wz1zFK',
-  
-    // Browser Storage Key
-    storageKey: 'storageKey@20xx',
-  
-    // Google developer console credentials
-    developerKey: 'AIzaSyDGM66BZhGSmBApm3PKL-xCrri-3Adb06I',
-    clientId: '971238950983-aef7kjl23994hjj9e8m5tch4a22b5dut.apps.googleusercontent.com',
-    apiKey: 'AIzaSyDGM66BZhGSmBApm3PKL-xCrri-3Adb06I',
-    clientSecret: 'erp6ZMRG6XFiMqHkjTDby2UI',
-    google_redirect_url: 'http://localhost:4200',
-    scope: [
-      'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/calendar',
-      'https://www.googleapis.com/auth/calendar.events'
-    ].join(' '),
-  
-    // Octo-doc URL
-    REAL_TIME_URL: 'localhost:3001',
-  
-    // Mailing URLs
-    MAILING_BASE_URL: 'http://localhost:2000',
-    MAILING_BASE_API_URL: 'http://localhost:8080/api/mails',
-  
-    // Authentication URLs
-    AUTH_BASE_URL: 'http://localhost:3000',
-    AUTH_BASE_API_URL: 'http://localhost:8080/api/auths',
-  
-    // Groups URLs
-    GROUPS_BASE_URL: 'http://localhost:4000',
-    GROUPS_BASE_API_URL: 'http://localhost:8080/api/groups',
-  
-    // Workspace URLs
-    WORKSPACE_BASE_URL: 'http://localhost:5000',
-    WORKSPACE_BASE_API_URL: 'http://localhost:8080/api/workspaces',
-  
-    // User URLs
-    USER_BASE_URL: 'http://localhost:7000',
-    USER_BASE_API_URL: 'http://localhost:8080/api/users',
-  
-    // Posts URLs
-    POST_BASE_URL: 'http://localhost:8000',
-    POST_BASE_API_URL: 'http://localhost:8080/api/posts',
-  
-    // Sockets URLs
-    SOCKETS_BASE_URL: 'ws://localhost:9000',
-    SOCKETS_BASE_API_URL: 'http://localhost:9000/api',
-  
-    // Utilities URLs
-    UTILITIES_BASE_URL: 'http://localhost:10000',
-  };
-  
+  // Browser Storage Key
+  storageKey: 'storageKey@20xx',
+
+  // Google developer console credentials 
+  developerKey: 'AIzaSyDGM66BZhGSmBApm3PKL-xCrri-3Adb06I',
+  clientId: '971238950983-aef7kjl23994hjj9e8m5tch4a22b5dut.apps.googleusercontent.com',
+  apiKey: 'AIzaSyDGM66BZhGSmBApm3PKL-xCrri-3Adb06I',
+  clientSecret: 'erp6ZMRG6XFiMqHkjTDby2UI',
+  google_redirect_url: 'http://localhost:4200',
+  scope: [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events'
+  ].join(' '),
+
+  // Base Client Url
+  clientUrl: 'http://localhost:4200',
+
+  // Octo-doc URL
+  REAL_TIME_URL: 'localhost:8080/editor',
+
+  // Mailing URLs
+  MAILING_BASE_URL: 'http://localhost:8080/mails',
+  MAILING_BASE_API_URL: 'http://localhost:8080/api/mails',
+
+  // Authentication URLs
+  AUTH_BASE_URL: 'http://localhost:8080/auths',
+  AUTH_BASE_API_URL: 'http://localhost:8080/api/auths',
+
+  // Groups URLs
+  GROUPS_BASE_URL: 'http://localhost:8080/groups',
+  GROUPS_BASE_API_URL: 'http://localhost:8080/api/groups',
+
+  // Workspace URLs
+  WORKSPACE_BASE_URL: 'http://localhost:8080/workspaces',
+  WORKSPACE_BASE_API_URL: 'http://localhost:8080/api/workspaces',
+
+  // User URLs
+  USER_BASE_URL: 'http://localhost:8080/users',
+  USER_BASE_API_URL: 'http://localhost:8080/api/users',
+
+  // Posts URLs
+  POST_BASE_URL: 'http://localhost:8080/posts',
+  POST_BASE_API_URL: 'http://localhost:8080/api/posts',
+
+  // Notifications URLs
+  NOTIFICATIONS_BASE_URL: 'ws://localhost:8080',
+  NOTIFICATIONS_BASE_API_URL: 'http://localhost:8080/api/notifications',
+
+  // Utilities URLs
+  UTILITIES_BASE_URL: 'http://localhost:8080/uploads',
+  UTILITIES_BASE_API_URL: 'http://localhost:8080/api/utilities'
+};
