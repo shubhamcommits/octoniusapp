615926ef554fc488449cf1c9e044cf539055c3d1
Cosmin Ciobanu <cosmin@octonius.com>
2020-05-26 18:13:21 +0300

    fixed nav and some new icons

--- client-upgrade/client/modules/folio/folio-header/folio-header.component.ts
+++ client-upgrade/client/modules/folio/folio-header/folio-header.component.ts
@@ -63,7 +63,7 @@ export class FolioHeaderComponent implements OnInit {
    * This function is responsible for taking the user back to their previous locations
    */
   goBackToFiles() {
-    this.router.navigate(['/dashboard', 'groups', this.groupId, 'files'])
+    this.router.navigate(['/dashboard', 'work', 'groups', 'files'], {queryParams: {group: this.groupId}})
 
     // Change the title of the tab
     this.titleService.setTitle('Octonius');
