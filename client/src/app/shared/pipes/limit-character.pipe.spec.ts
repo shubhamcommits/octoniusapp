eafa2551b647cdb69181c32da3e4850438646a4f
Shubham Singh <shubhamsingh@SHUBHAMs-MacBook-Pro.local>
2020-06-04 12:57:13 +0530

    pushing the empty state of master

--- client/src/app/shared/pipes/limit-character.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LimitCharacterPipe } from './limit-character.pipe';
-
-describe('LimitCharacterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LimitCharacterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
