593a2b3c6eb257bd4b85fac602c628eebce86b25
Mustafa Mahmutovic <mustafa.mahmutovic93@gmail.com>
2020-08-18 13:40:30 +0200

    Added guard for work

---
+++ client/src/shared/guards/group-guard/group-guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { StorageService } from 'src/shared/services/storage-service/storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GroupGuard implements CanActivate  {
+  constructor(
+    private storageService: StorageService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const currentGroup = state.root.queryParamMap.get('group');
+    const userData = this.storageService.getLocalData('userData');
+    if (userData._groups.includes(currentGroup)) {
+        return true;
+    } else {
+        this.router.navigate(['/dashboard/myspace/inbox']);
+        return false;
+    }
+  }
+}
