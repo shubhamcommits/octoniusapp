9599fe9b0c182f875de2af1429e22c0dd40ce1ab
Shubham Singh <vshubham5636@gmail.com>
2020-09-10 00:15:11 +0530

    intermediate push for google drive

--- client/modules/user/user-clouds/user-connected-clouds/user-connected-clouds.component.ts
+++ client/modules/user/user-clouds/user-connected-clouds/user-connected-clouds.component.ts
@@ -17,6 +17,7 @@ export class UserConnectedCloudsComponent implements OnInit {
 
   ngOnInit() {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     console.log(this.googleUser)
     // this.googleService.refreshGoogleToken().then(() => {
@@ -29,6 +30,8 @@ export class UserConnectedCloudsComponent implements OnInit {
     //       );
 =======
 >>>>>>> 9735d628... intermediate push for google drive
+=======
+>>>>>>> 1c17b755... intermediate push for google drive
 
     //     // we have set a time-interval of 30mins so as to refresh the access_token in the group
     //     setInterval(() => {
