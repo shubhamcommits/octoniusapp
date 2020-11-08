7ef45d7cf1297202510275ed6b84173fd924e423
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2020-11-08 17:43:12 +0100

    fix console error

--- client/src/app/common/shared/activity-feed/group-postbox/group-create-post-dialog-component/group-create-post-dialog-component.component.ts
+++ client/src/app/common/shared/activity-feed/group-postbox/group-create-post-dialog-component/group-create-post-dialog-component.component.ts
@@ -567,7 +567,7 @@ export class GroupCreatePostDialogComponent implements OnInit {
 
   async setAssignedBy(post) {
     this.postData = post;
-    if (this.postData.records.assignments) {
+    if (this.postData.records && this.postData.records.assignments && this.postData.records.assignments.length > 0) {
       this.postData.records.assignments = this.postData.records.assignments.sort((a1, a2) => (new Date(a1.date).getTime() < new Date(a2.date).getTime()) ? 1 : -1);
       this.lastAssignedBy = await this.publicFunctions.getOtherUser(this.postData.records.assignments[0]._assigned_from);
     }
