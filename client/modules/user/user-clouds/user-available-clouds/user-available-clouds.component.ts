d06609ac3fef0bf99bd4f52a57be8b007d594abb
khurram <khurram@codility.co>
2020-12-02 12:29:19 +0500

    Added modal for confirmation and changed redirect url for slack authentication.

--- client/modules/user/user-clouds/user-available-clouds/user-available-clouds.component.ts
+++ client/modules/user/user-clouds/user-available-clouds/user-available-clouds.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { UtilityService } from 'src/shared/services/utility-service/utility.service';
 
 @Component({
   selector: 'app-user-available-clouds',
@@ -7,7 +8,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class UserAvailableCloudsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private utilityService: UtilityService,
+  ) { }
 
   // Google User Output Event Emitter
   @Output('googleUser') googleUser = new EventEmitter();
@@ -15,8 +18,17 @@ export class UserAvailableCloudsComponent implements OnInit {
   ngOnInit() {
   }
 
-  emitGoogleUser(googleUser: any){
+  emitGoogleUser(googleUser: any) {
     this.googleUser.emit(googleUser)
   }
 
+  slackAuth() {
+    // Slack authentication permission.
+    this.utilityService.getConfirmDialogAlert("Do you allow?", "Octonius for Slack is requesting permission to use your Octonius account.", "Allow")
+      .then((result) => {
+        if (result.value) {
+          window.location.href ="https://slack.com/oauth/v2/authorize?client_id=2561616476.1145669381443&scope=channels:history,channels:join,channels:read,chat:write,chat:write.public,commands,emoji:read,groups:write,im:write,incoming-webhook,team:read,users:read,users:read.email,users:write&user_scope=channels:history,groups:history";
+        }
+      });
+  }
 }
