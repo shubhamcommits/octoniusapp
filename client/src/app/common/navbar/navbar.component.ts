b35bac4453e566ac44b541305dfa5efe4e38e853
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2020-12-07 09:29:00 +0100

    Fix navigation for admin & settings page

--- client/src/app/common/navbar/navbar.component.ts
+++ client/src/app/common/navbar/navbar.component.ts
@@ -87,11 +87,9 @@ export class NavbarComponent implements OnInit, AfterViewInit, OnDestroy {
     this.subSink.add(this.utilityService.routerStateData.subscribe((res) => {
       if (JSON.stringify(res) != JSON.stringify({})) {
         this.routerState = res['state']
-        /*
-        if (this.routerState === 'home') {
+        if (this.routerState === 'admin') {
           this.nextCommonNavbarState()
         } else
-        */
         if (this.routerState === 'group' || this.routerState === 'home') {
           this.nextGroupNavbarState()
 
