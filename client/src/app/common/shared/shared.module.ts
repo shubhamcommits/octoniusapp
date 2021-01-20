fa64e53567b75a204bd0c60dc916f943739ef58b
khurram <khurram@codility.co>
2021-01-20 16:36:51 +0500

    fixed the disabled users issue

--- client/src/app/common/shared/shared.module.ts
+++ client/src/app/common/shared/shared.module.ts
@@ -60,6 +60,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { PreviewFilesDialogComponent } from './preview-files-dialog/preview-files-dialog.component';
 import { NgxDocViewerModule } from 'ngx-doc-viewer';
@@ -246,6 +247,7 @@ import { MemberListMenuComponent } from './member-list-menu/member-list-menu.com
     MatMenuModule,
     MatBadgeModule,
     MatPaginatorModule,
+    MatExpansionModule
   ],
   exports: [
     AttachFilesComponent,
