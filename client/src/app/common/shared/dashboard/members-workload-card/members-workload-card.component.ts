29aa7e459d3993cc0bfc12ee5b56e06cbd9962ed
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2021-01-27 20:25:28 +0100

    First layout of the members workload table

--- client/src/app/common/shared/dashboard/members-workload-card/members-workload-card.component.ts
+++ client/src/app/common/shared/dashboard/members-workload-card/members-workload-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Injector } from '@angular/core';
-import { CalendarEvent, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
+import { CalendarEvent, CalendarEventTimesChangedEvent, CalendarView, CalendarWeekViewBeforeRenderEvent } from 'angular-calendar';
+import { addDays, addHours, endOfDay, endOfMonth, endOfWeek, isSaturday, isSunday, startOfDay, startOfWeek, subDays } from 'date-fns';
 import { PublicFunctions } from 'modules/public.functions';
 import moment from 'moment';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { GroupService } from 'src/shared/services/group-service/group.service';
 import { UserService } from 'src/shared/services/user-service/user.service';
+import { UtilityService } from 'src/shared/services/utility-service/utility.service';
 
 @Component({
   selector: 'app-members-workload-card',
@@ -21,50 +23,14 @@ export class MembersWorkloadCardComponent implements OnInit {
   groupMembers = [];
   groupTasks;
 
-  /*
-  period = 'this_week';
-  periods = [
-    {
-     key: 'this_week',
-     value: 'This week'
-    },
-    {
-     key: 'next_week',
-     value: 'Next week'
-    },
-    {
-     key: 'this_month',
-     value: 'This month'
-    },
-    {
-     key: 'next_month',
-     value: 'Next month'
-    }
+  //date for calendar Nav
+  dates: any = [];
+
+  monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
   ];
 
-  chartsReady = false;
-  */
-
-  // Define the month view
-  view: CalendarView = CalendarView.Week;
-
-  // Calendar Views
-  CalendarView = CalendarView;
-
-  // View Date
-  viewDate: Date = new Date();
-
-  // Open the current active day automatically
-  activeDayIsOpen: boolean = true;
-
-  // Modal Data to add the event and action
-  modalData: {
-    action: string;
-    event: CalendarEvent;
-  }
-
-  // Refresh Subject
-  refresh: Subject<any> = new Subject();
+  currentDate: any = new Date();
 
   // IsLoading behavior subject maintains the state for loading spinner
   public isLoading$ = new BehaviorSubject(false);
@@ -74,7 +40,8 @@ export class MembersWorkloadCardComponent implements OnInit {
 
   constructor(
     private injector: Injector,
-    private groupService: GroupService
+    private groupService: GroupService,
+    public utilityService: UtilityService
   ) { }
 
   async ngOnInit() {
@@ -103,195 +70,22 @@ export class MembersWorkloadCardComponent implements OnInit {
     // await this.assignTasks();
 
     // this.chartsReady = true;
+
+    ////////////////////
+    this.generateNavDate();
   }
 
-  /*
-  assignTasks() {
-
-    this.groupMembers.forEach(async (member, index) => {
-      const tasks = this.groupTasks.filter((task) => {
-        return task._assigned_to.some((a) => {
-          return a._id === member._id;
-        });
-      });
-      this.groupMembers[index].tasks = tasks;
-      this.groupMembers[index].tasksCount = tasks.length;
-
-      this.groupMembers[index].barChart = await this.getUserWorkStatisticsChartData(tasks);
-      this.groupMembers[index].topStatus = await this.getUserHighestStatusCount(this.groupMembers[index].barChart?.data);
-      // this.groupMembers[index].lineChart = await this.getUserVelocityChartData(tasks);
-    });
-  }
-
-  async getUserHighestStatusCount(taskCounts) {
-    let toDoCount = taskCounts[0];
-    let inProgressCount = taskCounts[1];
-    let doneCount = taskCounts[2];
-
-    let count = 0;
-    let statusName = 'to do';
-
-    if (toDoCount > inProgressCount) {
-      if (toDoCount > doneCount) {
-        count = toDoCount;
-        statusName = 'to do';
-      } else {
-        count = doneCount;
-        statusName = 'done';
-      }
-    } else if (inProgressCount > doneCount) {
-      count = inProgressCount;
-      statusName = 'in progress';
-    } else {
-      count = doneCount;
-      statusName = 'done';
-    }
-
-    return {
-      count: count,
-      name: statusName
-    };
-  }
-
-  private async getTasksStatisticsData(userTasks) {
-
-    let toDoCount = 0;
-    let inProgressCount = 0;
-    let doneCount = 0;
-    let overdueCount = 0;
-
-    const today = moment().subtract(1, 'days').endOf('day').format();
-
-    userTasks.forEach(task => {
-      if (task.task.status == 'to do') toDoCount++;
-      if (task.task.status == 'in progress') inProgressCount++;
-      if (task.task.status == 'done') doneCount++;
-      if (moment(task.task.due_to).isBefore(today)) overdueCount++;
-    });
-
-    return (this.period == 'this_week') ? [toDoCount, inProgressCount, doneCount, overdueCount] : [toDoCount, inProgressCount, doneCount];
-  }
-
-  async getUserWorkStatisticsChartData(tasks) {
-    const data = await this.getTasksStatisticsData(tasks);
-    const options = {
-      legend: {
-        display: false
-      },
-      scales: {
-          yAxes: [{
-              stacked: true,
-              display: false,
-              gridLines: {
-                  drawBorder: false,
-                  display: false,
-              },
-          }],
-          xAxes: [{
-              stacked: true,
-              display: false,
-              gridLines: {
-                  drawBorder: false,
-                  display: false,
-              }
-          }]
-      },
-    };
-
-    const colors =
-      (this.period == 'this_week')
-        ? [{
-          backgroundColor: [
-            '#FFAB00',
-            '#0bc6a0',
-            '#4a90e2',
-            '#FF6584'
-          ]
-        }]
-        : [{
-          backgroundColor: [
-            '#FFAB00',
-            '#0bc6a0',
-            '#4a90e2'
-          ]
-        }];
-    let plugins = [{
-      beforeDraw(chart, options) {
-
-      }
-    }];
-
-    return {
-      data: data,
-      options: options,
-      colors: colors,
-      type: 'bar',
-      labels: (this.period == 'this_week') ? ['To Do', 'In Progress', 'Done', 'Overdue'] : ['To Do', 'In Progress', 'Done'],
-      plugins: plugins
+  async generateNavDate() {
+    for (var i = 0; i < 7; i++) {
+      const date = addDays(startOfWeek(new Date()), i);
+      this.dates.push({ day: date.getDate(), date: date, month: date.getMonth(), isweekend: (isSaturday(date) || isSunday(date)) });
     }
   }
 
-  async periodSelected(event) {
-    // Starts the spinner
-    this.isLoading$.next(true);
-
-    this.period = event.value;
-    this.userData.stats.group_dashboard_members_period = this.period;
-
-    this.chartsReady = false;
-
-    await this.initTable();
-
-    // User service
-    const userService = this.injector.get(UserService);
-
-    // Update user´s period
-    userService.updateUser(this.userData);
-    this.publicFunctions.sendUpdatesToUserData(this.userData);
-
-    // Stops the spinner and return the value with ngOnInit
-    this.isLoading$.next(false);
-  }
-  */
-
-  closeOpenMonthViewDay() {
-    this.activeDayIsOpen = false;
-  }
-
-  eventTimesChanged({
-    event,
-    newStart,
-    newEnd
-  }: CalendarEventTimesChangedEvent): void {
-    this.groupTasks = this.groupTasks.map(iEvent => {
-      if (iEvent === event) {
-        return {
-          ...event,
-          start: newStart,
-          end: newEnd
-        };
-      }
-      return iEvent;
-    });
-    this.handleEvent('Dropped or resized', event);
-  }
-
-  handleEvent(action: string, event: any): void {
-    this.modalData = { event, action };
-  }
-
-  beforeViewRender($event) {
-console.log($event);
-    const group: any[] = [];
-    let i = 0;
-    $event.period.events.forEach((event: any) => {
-        if (i % 2 === 0) {
-          group.push(event);
-        }
-        i++;
-    });
-
-    $event.period["eventGroups"] = Object.entries(group);
-console.log($event);
+  /**
+   * This function is responsible for opening a fullscreen dialog to see the member profile
+   */
+  openFullscreenModal(userId: string): void {
+    this.utilityService.openFullscreenModal(userId);
   }
 }
--- client/src/app/common/shared/shared.module.ts
+++ client/src/app/common/shared/shared.module.ts
@@ -100,9 +100,8 @@ import { MembersWorkloadCardComponent } from './dashboard/members-workload-card/
 import { SectionStatusCardComponent } from './dashboard/section-status-card/section-status-card.component';
 import { MemberDialogComponent } from './member-dialog/member-dialog.component';
 
-import { CalendarModule as Calendar, DateAdapter } from 'angular-calendar';
+import { CalendarModule as Calendar, CalendarUtils, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
 
 @NgModule({
   declarations: [
@@ -260,7 +259,7 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     Calendar.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    }),
+    })
   ],
   exports: [
     AttachFilesComponent,
