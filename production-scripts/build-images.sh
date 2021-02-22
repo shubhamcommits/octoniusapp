fd46f89ddda79d8cab486176a449b01af5c7fa5d
Shubham Singh <vshubham5636@gmail.com>
2021-02-22 19:52:07 +0530

    changing the build scripts

--- production-scripts/build-images.sh
+++ production-scripts/build-images.sh
@@ -8,7 +8,6 @@ source .env
 set +o allexport
 
 # build the Docker image (this will use the Dockerfile in the root of the repo)
-        docker build -t $CLIENT_IMAGE_NAME --compress=true --force-rm=true ../client
         docker build -t $MAILS_IMAGE_NAME --compress=true --force-rm=true ../services/mailing/server
         docker build -t $AUTHS_IMAGE_NAME --compress=true --force-rm=true ../services/authentication/server
         docker build -t $GROUPS_IMAGE_NAME --compress=true --force-rm=true ../services/groups/server
@@ -20,17 +19,16 @@ set +o allexport
         docker build -t $FOLIO_IMAGE_NAME --compress=true --force-rm=true ../services/folio/server
         docker build -t $SEARCH_IMAGE_NAME --compress=true --force-rm=true ../services/search/server
         docker build -t $INTEGRATIONS_IMAGE_NAME --compress=true --force-rm=true ../services/integrations/server
+        docker build -t $CLIENT_IMAGE_NAME --compress=true --force-rm=true ../client
         docker build -t $NGINX_IMAGE_NAME --compress=true --force-rm=true ../nginx
 
 # Clear unnecessary Image build
         docker image prune -f 
 
-
 # authenticate with the Docker Hub registry
         docker login
 
 # push the new Docker image to the Docker registry
-        docker push $CLIENT_IMAGE_NAME
         docker push $MAILS_IMAGE_NAME
         docker push $AUTHS_IMAGE_NAME
         docker push $GROUPS_IMAGE_NAME
@@ -42,4 +40,5 @@ set +o allexport
         docker push $FOLIO_IMAGE_NAME
         docker push $SEARCH_IMAGE_NAME
         docker push $INTEGRATIONS_IMAGE_NAME
+        docker push $CLIENT_IMAGE_NAME
         docker push $NGINX_IMAGE_NAME
--- production-scripts/pull-images.sh
+++ production-scripts/pull-images.sh
@@ -2,59 +2,25 @@
 
 # Octonius Pull Docker Images
 
-# Client Microservice Image Name
-export CLIENT_IMAGE_NAME=octoniusapp/octonius-prod:client
+# Import Environment Variables
+set -o allexport
+source .env
+set +o allexport
 
-# Mails Microservice Image Name
-export MAILS_IMAGE_NAME=octoniusapp/octonius-prod:mailing-server
+# Authenticate with the Docker Hub registry
+docker login
 
-# Auths Microservice Image Name
-export AUTHS_IMAGE_NAME=octoniusapp/octonius-prod:auths-server
-
-# Groups Microservice Image Name
-export GROUPS_IMAGE_NAME=octoniusapp/octonius-prod:groups-server
-
-# Workspaces Microservice Image Name
-export WORKSPACES_IMAGE_NAME=octoniusapp/octonius-prod:workspaces-server
-
-# Users Microservice Image Name
-export USERS_IMAGE_NAME=octoniusapp/octonius-prod:users-server
-
-# Posts Microservice Image Name
-export POSTS_IMAGE_NAME=octoniusapp/octonius-prod:posts-server
-
-# Notifications Microservice Image Name
-export NOTIFICATIONS_IMAGE_NAME=octoniusapp/octonius-prod:notifications-server
-
-# Utilities Microservice Image Name
-export UTILITIES_IMAGE_NAME=octoniusapp/octonius-prod:utilities-server
-
-# Folio Microservice Image Name
-export FOLIO_IMAGE_NAME=octoniusapp/octonius-prod:folio-server
-
-# Search Microservice Image Name
-export SEARCH_IMAGE_NAME=octoniusapp/octonius-prod:search-server
-
-# Integrations Microservice Image Name
-export INTEGRATIONS_IMAGE_NAME=octoniusapp/octonius-prod:integrations-server
-
-# Nginx Image Name
-export NGINX_IMAGE_NAME=octoniusapp/octonius-prod:nginx
-
-# authenticate with the Docker Hub registry
-          docker login
-
-# pull the new Docker image to the Docker registry
-        docker pull $CLIENT_IMAGE_NAME
-        docker pull $MAILS_IMAGE_NAME
-        docker pull $AUTHS_IMAGE_NAME
-        docker pull $GROUPS_IMAGE_NAME
-        docker pull $WORKSPACES_IMAGE_NAME
-        docker pull $USERS_IMAGE_NAME
-        docker pull $POSTS_IMAGE_NAME
-        docker pull $NOTIFICATIONS_IMAGE_NAME
-        docker pull $UTILITIES_IMAGE_NAME
-        docker pull $FOLIO_IMAGE_NAME
-        docker pull $SEARCH_IMAGE_NAME
-        docker pull $INTEGRATIONS_IMAGE_NAME
-        docker pull $NGINX_IMAGE_NAME
+# Pull the new Docker image to the Docker registry
+          docker pull $MAILS_IMAGE_NAME
+          docker pull $AUTHS_IMAGE_NAME
+          docker pull $GROUPS_IMAGE_NAME
+          docker pull $WORKSPACES_IMAGE_NAME
+          docker pull $USERS_IMAGE_NAME
+          docker pull $POSTS_IMAGE_NAME
+          docker pull $NOTIFICATIONS_IMAGE_NAME
+          docker pull $UTILITIES_IMAGE_NAME
+          docker pull $FOLIO_IMAGE_NAME
+          docker pull $SEARCH_IMAGE_NAME
+          docker pull $INTEGRATIONS_IMAGE_NAME
+          docker pull $CLIENT_IMAGE_NAME
+          docker pull $NGINX_IMAGE_NAME
