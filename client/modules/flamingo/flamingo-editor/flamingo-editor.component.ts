33e7ca368b194434ff2b3bee6f309a7fbd529a60
khurram <khurram@codility.co>
2021-04-14 16:28:35 +0500

    dropdown added options dynamically

--- client/modules/flamingo/flamingo-editor/flamingo-editor.component.ts
+++ client/modules/flamingo/flamingo-editor/flamingo-editor.component.ts
@@ -38,7 +38,47 @@ export class FlamingoEditorComponent implements OnInit {
     this.questions.push({
       type: "Dropdown",
       text: "Where are you from?",
-      options:[]
+      options:["new text"]
+    })
+    this.questions.push({
+      type: "Welcome",
+      text: "Welcome to new Survey?",
+      options:["new text"]
+    })
+    this.questions.push({
+      type: "Thanks",
+      text: "Thank you for your time!!!",
+      options:["new text"]
+    })
+    this.questions.push({
+      type: "ShortText",
+      text: "What is your name?"
+    })
+    this.questions.push({
+      type: "Yes/No",
+      text: "Is this your first job?"
+    })
+    this.questions.push({
+      type: "Scale",
+      text: "How much you are satisfied by the service?",
+      scale:{
+        size: 11,
+      }
+    })
+    this.questions.push({
+      type: "Dropdown",
+      text: "Where are you from?",
+      options:["new text"]
+    })
+    this.questions.push({
+      type: "Welcome",
+      text: "Welcome to new Survey?",
+      options:["new text"]
+    })
+    this.questions.push({
+      type: "Thanks",
+      text: "Thank you for your time!!!",
+      options:["new text"]
     })
     this.activeQuestion = this.questions[this.activeQuestionIndex];
     console.log("questions list",this.questions);
@@ -57,10 +97,11 @@ changeActiveIndex(index:number){
 }
 
 changeOptionText(event:any,i:number,j:number){
-    if(!j){
+  if(j == null){
       this.questions[i].options.push(this.newOption);
       this.newOption='';
-    }
+  }else if(j!= null){
+    this.questions[i].options[j] = event.target.value;
+  }
 }
-
 }
