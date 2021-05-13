b5fe1e04eb1c28705498f4778856d7d9fcd59a5f
Shubham Singh <vshubham5636@gmail.com>
2021-05-13 15:41:18 +0530

    changing the dev urls for mgmt portal

--- services/users/server/configs/development.config.ts
+++ services/users/server/configs/development.config.ts
@@ -94,7 +94,7 @@ function devConfigInit() {
   process.env.FLAMINGO_SERVER = `${process.env.PROTOCOL}://${process.env.DOMAIN}:${process.env.FLAMINGO_PORT}`
   process.env.FLAMINGO_SERVER_API = `${process.env.PROTOCOL}://${process.env.DOMAIN}:${process.env.FLAMINGO_PORT}/api`
 
-  process.env.MANAGEMENT_URL = 'https://management.octonius.com'
+  process.env.MANAGEMENT_URL = 'http://localhost:3300'
   process.env.MANAGEMENT_API_KEY = "TZCDAC3CDCJILSRGA2II"
 };
 
