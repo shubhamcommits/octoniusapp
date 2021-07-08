a0c493911f317896e3ea8312fbde2e7aaf67918c
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2021-07-08 12:22:14 +0200

    Make sendgrid calls from mgmt

--- services/mailing/server/api/routes/mails.routes.ts
+++ services/mailing/server/api/routes/mails.routes.ts
@@ -17,31 +17,28 @@ routes.post('/new-workspace', sendMail.newWorkspace);
 routes.post('/group-joined', sendMail.joinedGroup);
 
 // POST - User Mentioned Post
-routes.post('/user-post-mention', sendMail.userMentionedPost);
+// routes.post('/user-post-mention', sendMail.userMentionedPost);
 
 // POST - User Mentioned Comment
-routes.post('/user-comment-mention', sendMail.userMentionedComment);
+// routes.post('/user-comment-mention', sendMail.userMentionedComment);
 
 //POST - Task Assigned
-routes.post('/task-assigned', sendMail.taskAssigned);
+// routes.post('/task-assigned', sendMail.taskAssigned);
 
 //POST - Task Reminder
-routes.post('/task-reminder', sendMail.scheduleTaskReminder);
+// routes.post('/task-reminder', sendMail.scheduleTaskReminder);
 
 // POST - Event Reminder
-routes.post('/event-reminder', sendMail.scheduleEventReminder);
+// routes.post('/event-reminder', sendMail.scheduleEventReminder);
 
 // POST - Event Assigned
-routes.post('/event-assigned', sendMail.eventAssigned);
+// routes.post('/event-assigned', sendMail.eventAssigned);
 
 // POST - Workspace Joined
 routes.post('/join-workspace', sendMail.joinWorkspace);
 
 // POST - Task Reassigned
-routes.post('/task-reassign', sendMail.taskReassigned);
-
-// POST - User Completed Task
-routes.post('/task-complete', sendMail.userCompletedTask);
+// routes.post('/task-reassign', sendMail.taskReassigned);
 
 // POST - Join Workplace
 routes.post('/invite-user', sendMail.joinWorkspace);
