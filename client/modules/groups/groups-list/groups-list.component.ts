0df0db46f59420d84adaa132f9250fcbdf6c0781
Shubham Singh <vshubham5636@gmail.com>
2021-07-09 14:14:58 +0530

    reverting the groups count

--- client/modules/groups/groups-list/groups-list.component.ts
+++ client/modules/groups/groups-list/groups-list.component.ts
@@ -200,7 +200,7 @@ export class GroupsListComponent implements OnInit {
    */
    getUniqueMembersCount(members, admins) {
 
-    if (members.length > 0 && admins.length > 0) {
+    if (members.length >= 0 && admins.length >= 0) {
       // Merge the Admin and Members array
       Array.prototype.push.apply(members, admins)
 
