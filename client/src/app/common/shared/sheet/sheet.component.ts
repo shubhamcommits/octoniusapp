ac18d98e8fa7925f1b3b85d07ad753b1500594a0
Shubham Singh <vshubham5636@gmail.com>
2021-07-13 17:10:35 +0530

    tables added and css has been improved

--- client/src/app/common/shared/sheet/sheet.component.ts
+++ client/src/app/common/shared/sheet/sheet.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Injector, Input, OnInit, ViewChild } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table'
+import { ActivatedRoute } from '@angular/router'
+import { BehaviorSubject } from 'rxjs'
 
 import * as XLSX from 'xlsx'
 
@@ -11,85 +17,122 @@ type AOA = any[][]
 })
 export class SheetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _Injector: Injector,
+    public _Dialog: MatDialog,
+    private _ActivatedRoute: ActivatedRoute,
+  ) { }
 
   // Raw Data of Sheet
-  @Input('data') data: AOA = []
+  @Input('data') data: any = []
 
   wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' }
-  
+
   fileName: string = 'SheetJS.xlsx'
 
+  // File 
   @Input('file') file: any
 
+  // File URL
   @Input('fileUrl') fileUrl: any
 
-  ngOnInit(): void {
-    console.log(this.file)
-    console.log(this.fileUrl)
-  }
+  // Is loading behaviour
+  isLoading$ = new BehaviorSubject(false)
 
-  ngOnChanges(){
-    console.log(this.file)
-    console.log(this.fileUrl)
-  }
+  // Columns
+  displayedColumns: any = []
 
+  // Datasource
+  dataSource = new MatTableDataSource([])
 
-  onFileChange(evt: any) {
-    /* wire up file reader */
-    const target: DataTransfer = <DataTransfer>(evt.target)
+  // Sort Table
+  @ViewChild(MatSort, { static: true }) sort: MatSort
 
-    console.log(target.files[0])
+  // Paginator
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
 
-    if (target.files.length !== 1) throw new Error('Cannot use multiple files')
-    const reader: FileReader = new FileReader()
-    reader.onload = (e: any) => {
-      /* read workbook */
-      const bstr: string = e.target.result
-      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' })
+  ngOnInit(): void {}
 
-      /* grab first sheet */
-      const wsname: string = wb.SheetNames[0]
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname]
-
-      /* save data */
-      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }))
+  async ngOnChanges() {
+    if (this.fileUrl) {
+      let blob = await this.getBlobFromUrl(this.fileUrl)
+      let data = await this.getDataFromBlob(blob)
       console.log(this.data)
+      this.calculateData()
     }
-    reader.readAsBinaryString(target.files[0])
   }
 
-  previewFile(file) {
-
-    var reader  = new FileReader();
-  
-    reader.onloadend = (e: any) => {
-      /* read workbook */
-      const bstr: string = e.target.result
-      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' })
-
-      /* grab first sheet */
-      const wsname: string = wb.SheetNames[0]
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname]
-
-      /* save data */
-      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }))
-      console.log(this.data)
-    }
-    reader.readAsDataURL(file);
+  /**
+   * Calculate Data Function
+   */
+  async calculateData(){
+    this.populateDatasource(this.data)
   }
 
-  export(fileName: string): void {
-    /* generate worksheet */
-    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data)
-
-    /* generate workbook and add the worksheet */
-    const wb: XLSX.WorkBook = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
-
-    /* save to file */
-    XLSX.writeFile(wb, fileName)
+  /**
+   * This function populates the dataset
+   * @param dataSet 
+   */
+  populateDatasource(dataSet: any) {
+    this.dataSource = new MatTableDataSource(dataSet)
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
   }
 
+  /**
+   * Filter result function
+   * @param value 
+   * @param event 
+   */
+  public filterResults = (value: string, event: any) => {
+    if (event.keyCode === 13)
+      this.dataSource.filter = value.trim().toLocaleLowerCase()
+  }
+
+  /**
+   * This function is responsible for creating blob from the FILE
+   * @param myImageUrl 
+   * @returns 
+   */
+  getBlobFromUrl(myImageUrl) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      request.open('GET', myImageUrl, true);
+      request.responseType = 'blob';
+      request.onload = () => {
+        resolve(request.response);
+      };
+      request.onerror = reject;
+      request.send();
+    })
+  }
+
+  /**
+   * This function is responsible for mapping the blob data into binary file and prepare the data for table
+   * @param blob 
+   * @returns 
+   */
+  getDataFromBlob(blob: any) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        /* read workbook */
+        const bstr: string = e.target.result
+        const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' })
+
+        /* grab first sheet */
+        const wsname: string = wb.SheetNames[0]
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname]
+
+        /* save data */
+        this.data = (XLSX.utils.sheet_to_json(ws, { header: 1 }))
+        this.displayedColumns = (XLSX.utils.sheet_to_json(ws, { header: 1 })[0])
+
+        resolve(reader.result)
+      };
+      reader.onerror = reject;
+      reader.readAsBinaryString(blob)
+    })
+  }
 
 }
