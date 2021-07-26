dd7b0770172192e4e0a35bac98a23fc3500dc1bf
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2021-07-26 13:05:16 +0200

    ng update angular

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -55,7 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 // Imported Process to be able to support the implementation of nextTick in the Browserify bundle
 import * as process from 'process';
