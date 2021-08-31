bd9439b05106a5ba231ed9b486a9a6d5c806c889
root <root@ip-172-31-14-137.ap-south-1.compute.internal>
2021-08-31 19:28:02 +0000

    updated mongodb cluster setup

---
+++ scripts/acceptance/mongo_setup_scripts/setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+MONGODB1=mongo1
+MONGODB2=mongo2
+MONGODB3=mongo3
+echo "**********************************************" ${MONGODB1}
+echo "Waiting for startup.."
+until curl http://${MONGODB1}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+  printf '.'
+  sleep 1
+done
+
+# echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
+# echo "Started.."
+
+
+echo SETUP.sh time now: `date +"%T" `
+mongo --host mongo1:27017 <<EOF
+var cfg = {
+    "_id": "rs0",
+    "protocolVersion": 1,
+    "version": 1,
+    "members": [
+        {
+            "_id": 0,
+            "host": "mongo1:27017"
+        },
+        {
+            "_id": 1,
+            "host": "mongo2:27017",
+        },
+        {
+            "_id": 2,
+            "host": "mongo3:27017",
+            "priority": 0
+        }
+    ],settings: {chainingAllowed: true}
+};
+rs.initiate(cfg, { force: true });
+rs.reconfig(cfg, { force: true });
+rs.secondaryOk();
+db.getMongo().setReadPref('nearest');
+db.getMongo().setSlaveOk();
+EOF
