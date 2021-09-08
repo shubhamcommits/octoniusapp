02d34ac706cf4dd247f128bbde8d5ca7a40182d8
root <root@ip-172-31-14-137.ap-south-1.compute.internal>
2021-09-08 08:14:41 +0000

    update backup and restore file

--- scripts/acceptance/backup.sh
+++ scripts/acceptance/backup.sh
@@ -3,32 +3,31 @@
 # Backup source DB 
 echo " backup db "
 
-echo " Is your mongoDB has auth enable type True Or false "
-read auth
-echo " Your MongoDB has Auth enable=$auth"
+#echo " Is your mongoDB has auth enable type True Or false "
+#read auth
+#echo " Your MongoDB has Auth enable=$auth"
+#echo "Enter MongoDB Host "
+#read host
+#echo "MongoDB Host is $host
+#echo "Enter Port :
+#read port
+#echo "Enter DB name "
+#read dbname
+#echo " Database Name Is $dbname"
 
-echo "Enter MongoDB Host "
-read host
-echo "MongoDB Host is $host
-echo "Enter Port :
-read port
-echo "Enter DB name "
-read dbname
-echo " Database Name Is $dbname"
+#if [[ "$auth" == "true" ]]; then
+#
+#	#echo " Enter UserName for DB "
+	#read username
+	#echo "The User Name is $username"
+	#echo
+	#echo "Enter the Password for source DB "
+	#read password
+	#echo "The Password is  $password"
+	#mongodump --host $host:$port -u $username -p $password --forceTableScan --db=$dbname --out=/tmp/octonius_dump
+       #echo " successfully backup the database and backup location is = /tmp/octonius_dump "
 
-if [[ "$auth" == "true" ]]; then
-
-	echo " Enter UserName for DB "
-	read username
-	echo "The User Name is $username"
-	echo
-	echo "Enter the Password for source DB "
-	read password
-	echo "The Password is  $password"
-	mongodump --host $host:$port -u $username -p $password --forceTableScan --db=$dbname --out=/tmp/octonius_dump
-       echo " successfully backup the database and backup location is = /tmp/octonius_dump "
-
-else
-        mongodump --host $host:$port --forceTableScan --db=$dbname --out=/tmp/octonius_dump
+#else
+        mongodump --host mongodb:27017 --forceTableScan --db=octonius --out=/tmp/octonius_dump
          echo " successfully backup the database and backup location is = /tmp/octonius_dump "
 fi
