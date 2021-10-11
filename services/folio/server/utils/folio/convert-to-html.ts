48ddea504c3c3e52e523623f26b28433ea33c024
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2021-10-11 22:07:23 +0200

    add the option to run the soffice command in macos

--- services/folio/server/utils/folio/convert-to-html.ts
+++ services/folio/server/utils/folio/convert-to-html.ts
@@ -3,7 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const createHtml = async (file_uri, toFormate="html:HTML:EmbedImages") => {
-    let sofficeCommand = "soffice" // default for linux
+    let sofficeCommand = "soffice"; // default for linux
+    switch (process.platform) {
+        case 'darwin': sofficeCommand = '/Applications/LibreOffice.app/Contents/MacOS/soffice';
+            break;
+        default:
+            return; // callback(new Error(`Operating system not yet supported: ${process.platform}`));
+    }
 
     return new Promise( (resolve, reject) => {
         exec(`${sofficeCommand} --convert-to ${toFormate} --outdir "${process.env.FILE_UPLOAD_FOLDER}" "${file_uri}"`, (error, stdout, stderr) => {
