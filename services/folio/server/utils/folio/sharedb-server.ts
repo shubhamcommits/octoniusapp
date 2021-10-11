cac38cdca41d8994df39f1d7f0c67003904d73df
prasana shinde <prasana.shinde@acc.ltd>
2021-10-11 21:38:33 +0530

    refactor backend import/export to work with latest node version

--- services/folio/server/utils/folio/sharedb-server.ts
+++ services/folio/server/utils/folio/sharedb-server.ts
@@ -2,7 +2,7 @@ import ShareDB from 'sharedb'
 
 ShareDB.types.register(require('rich-text').type);
 
-module.exports = new ShareDB({
+export default new ShareDB({
     db: require('sharedb-mongo')(process.env.DB_URL || 'mongodb://127.0.0.1:27017/octonius?auto_reconnect=true', {
         useUnifiedTopology: true,
         useNewUrlParser: true
