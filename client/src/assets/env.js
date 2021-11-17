b775995c8da123aa34633ee04b85fabde62b09eb
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2021-11-17 21:07:28 +0100

    Fix env properties for production

--- client/src/assets/env.js
+++ client/src/assets/env.js
@@ -11,4 +11,7 @@
     window["env"]["active_directory_authority_cloud_id"] = "";
     window["env"]["active_directory_redirect_url"] = "";
     window["env"]["LDAP_METHOD"] = "";
+    window["env"]["SSO_GOOGLE_METHOD=GOOGLE"] = "";
+    window["env"]["GOOGLE_CLIENT_ID"] = "";
+    window["env"]["GOOGLE_CLIENT_SECRET"] = "";
   })(this);
