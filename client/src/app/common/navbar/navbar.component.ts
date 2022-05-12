7b8e4001ae7f9eae930271330c7c0a54e740a163
Juan Alvarez Martinez <juan.am.itc@gmail.com>
2022-05-12 08:24:57 +0200

    reorder languages in menu

--- client/src/app/common/navbar/navbar.component.ts
+++ client/src/app/common/navbar/navbar.component.ts
@@ -252,9 +252,9 @@ console.log(this.locale);
       this.selectLanguage(this.userData.locale);
     }
 
-    this.languages.push({ name: $localize`:@@navbar.spanish:Spanish`, code: 'es'});
-    this.languages.push({ name: $localize`:@@navbar.english:English`, code: 'en'});
     this.languages.push({ name: $localize`:@@navbar.english:German`, code: 'de'});
+    this.languages.push({ name: $localize`:@@navbar.english:English`, code: 'en'});
+    this.languages.push({ name: $localize`:@@navbar.spanish:Spanish`, code: 'es'});
   }
 
   selectLanguage(languageCode: any) {
