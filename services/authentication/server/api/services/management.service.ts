67e152bc2406e647391598b667c3984ad9587d8d
Juan Alvarez <juan@octonius.com>
2022-06-08 10:01:19 +0200

    Allow to create workspaces without mgmt portal

--- services/authentication/server/api/services/management.service.ts
+++ services/authentication/server/api/services/management.service.ts
@@ -9,13 +9,24 @@ export class ManagementService {
     MANAGEMENT_BASE_API_URL = process.env.MANAGEMENT_URL + '/api';
 
     callNewWorkplacesAvailable(environment: string) {
-        try {
+        if (process.env.NODE_ENV == 'development') {
+            return new Promise((resolve, reject) => {
+                resolve({
+                    data: {
+                        message: 'Creation of New Workplaces availability is true',
+                        status: true
+                    }
+                });
+            });
+        } else {
             return axios.get(`${this.MANAGEMENT_BASE_API_URL}/workspace/blockNewWorkplaces`, {
                 params: {
                     environment: environment
-                },
-                
+                }
             });
+        }
+        try {
+            
 
         } catch (err) {
             throw (err);
