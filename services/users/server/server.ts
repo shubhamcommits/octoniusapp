7f4b74a9b9cd7eb5d03536399391220b56fb74b1
Juan Alvarez <juan@octonius.com>
2022-10-26 18:10:58 +0000

    server.ts edited online with Bitbucket

--- services/users/server/server.ts
+++ services/users/server/server.ts
@@ -35,7 +35,7 @@ if (cluster.isMaster) {
 } else {
 
   // Define Users Application port
-  const port = 7000;
+  const port = process.env.PORT || 7000;
 
   // Defining the Host Name
   const host: any = process.env.HOST || '0.0.0.0';
