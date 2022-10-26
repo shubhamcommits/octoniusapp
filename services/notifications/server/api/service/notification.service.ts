316f780d835356869d150fbe31da52f009e30793
Juan Alvarez <juan@octonius.com>
2022-10-27 00:02:08 +0200

    refactor sendFirebaseNotifications

--- services/notifications/server/api/service/notification.service.ts
+++ services/notifications/server/api/service/notification.service.ts
@@ -1,23 +1,7 @@
 import { Notification, User, File, Workspace, Group, Post, Story } from "../models";
 import { Readable } from 'stream';
-import { helperFunctions, axios } from '../../utils';
+import { helperFunctions, axios, firebaseNotifications } from '../../utils';
 import moment from "moment";
-
-var admin = require("firebase-admin");
-const serviceAccount = require("./octonius-mobile-firebase-adminsdk.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-    // credential: cert({
-    //     projectId: 'octonius-mobile',
-    //     clientEmail: "firebase-adminsdk-h61te@octonius-mobile.iam.gserviceaccount.com",
-    //     privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDBZybr5UQReHtG\n94Rg8b0nq4LJWUegTqdnBcBph3OoDEnEE6BgXFySdxKuXlj5202sMqpUqTa84Lzy\nYfso/3K/Uu9qdFQN6kec3L0ZC7VbltVeNKYFU+3qP91l0D6dg5W78Djh3sEOMfI0\no7f/wQxFFW3vYECvuNIpa4a+jOSwvLxqstoW6pWOrku935+5TciijOefOjrnEAgR\ng9wC+Y+bLjfZXVYL01EElY1q/HJYMK7gnFcZDhbxjBNINA40zrNnss0H8v1WyJ3M\nhIi9lab3BUycQWHfkP31CoZjOHNYQ/DsIH6acCAxqCVxBCkHPgpWY35vTcfQVRhG\nohkYQ3kDAgMBAAECggEADFiA2AsdEr5tpCSVuziDOh6NLTw5QcbzkbSAyVu/qskK\nBm24TZ5nvGPEegQEywJQTX84d0oL9/eiqBhQF5hi40qwhMujN/YuFxnSKmCgdZXF\nzgsvU+S9Kbhk8iRecuCc9M/LWcPF8BmJHAoda0KP/XL2PxMWXgE+zS0Tf2NwD8YH\n/7/FBzisEf6sXnYOD8q6cSG9mzBl5fOFw9V8sP68UzXD6Pwhkx3O7aM4HHod8zhd\nX1kDqS9o5zV2Mzuku1DilMEmorQa20yuWahxv5ewPlvRhiiY0SQeHokwjjucEk3J\nJZ4ohJTLgvSQV3yHe5LgdVf2pdBvHJSDI3KOEguWVQKBgQDydmNZoUhsFt/4PDmn\nV8ikg7etMnDdlrf5wEolawuElObmLqzZ4kyLlSSrhgkr9nhtHE8kJSQOWwL3iUps\nDIQ0Oh6qWjVk+viHsdGk6gmiX2kPdJ4a5eQtbHrc6uvbLwcm5obiqf8PCn/vToVP\n9QnR5EqRjSKOvglCI8nQ470/hwKBgQDMM4lvNCUYwIydDUt+zN11ZJyzvnK1zs2/\nPAG4ZlZrGbt8dK8qbJH/U7YrAfjovHMKzXzGoR20l/tcYk6StWzNOIxukQ6cV47A\n/rc1szXeWAb3XanjN2wt19FJIGqPanJ1Tm5t6FhW5bzE9KOwGKNIjncKVK+mDhD8\nedGMvCYBpQKBgQDSnqjbGVFyI8TXPGnQxl7TGmCaIXEN7HlQiQtfycctmrOhTPZ2\nJzDbJ+m83ihleitOjQLqoSDbH5BKO4bcqVrGi55L2ST83U67gWpd2bgYxsza0jDt\nqLo4Az4PXjsYIZgS4LpXd9jK1hIgbZM8y92F6Mwl9/YHDWm5fKE5xjuFTQKBgQCu\ns2Tga+dU/t8OOmKdkB3jonliWgx/uPdTpb3/CibjKDe76YQ3Mn3RyMewkdZnH1r5\nIgafVRY5/FEDn+ODJo54IOocaiPq5Anw2brayYDLwdnr5glDqJX3vo2CF6azHing\nbIKTq1Vwuso+YuJr9Rg1KhV0FDHWSnCD4KDD2/BUNQKBgAx3Ug8F8rkcRjbB4tjx\n0Atzt9HFIHG32V5NMG/HY8uQc7tE8gtVpWt2YonTzqXlXguQ1wuxRvGxW8WHG6GX\nTo1IA4LFlJ8NmbRt+lnuNFo2dq7M5ClQ7DPDvhoHYG/p1QRUbMFlVHDLThPq3IpH\njqplpBP9mSAIFIAK6KPuMf4y\n-----END PRIVATE KEY-----\n"
-    // }),
-    //credential: applicationDefault(),
-    //credential: admin.credential.applicationDefault(),
-    //credential: admin.credential.cert(serviceAccount),
-    //credential: admin.credential.cert("./octonius-mobile-firebase-adminsdk.json"),
-    //databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-});
 
 /*  ===============================
  *  -- NOTIFICATIONS Service --
@@ -258,7 +242,7 @@ export class NotificationsService {
                     const actor = await User.findById({ _id: comment?._commented_by }).select('first_name last_name').lean();
 
                     if (owner.integrations.firebase_token) {
-                        this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Mention in Comment', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a comment.');
+                        firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Mention in Comment', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a comment.');
                     }
                 }
             });
@@ -305,7 +289,7 @@ export class NotificationsService {
                     const actor = await User.findById({ _id: (posted_by._id || posted_by) }).select('first_name last_name').lean();
 
                     if (owner.integrations.firebase_token) {
-                        this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a event.');
+                        firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a event.');
                     }
                 }
             });
@@ -352,7 +336,7 @@ export class NotificationsService {
                     const actor = await User.findById({ _id: (posted_by._id || posted_by) }).select('first_name last_name').lean();
 
                     if (owner.integrations.firebase_token) {
-                        this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Mention', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a post.');
+                        firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Mention', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a post.');
                     }
                 }
             })
@@ -403,7 +387,7 @@ export class NotificationsService {
 
                     if (user.integrations.firebase_token) {
                         // Send the notification to firebase for mobile notify
-                        this.sendFirebaseNotification(user._workspace._id || user._workspace, user.integrations.firebase_token, 'Octonius - New Mention', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a folio.');
+                        firebaseNotifications.sendFirebaseNotification(user._workspace._id || user._workspace, user.integrations.firebase_token, 'Octonius - New Mention', actor?.first_name + ' ' + actor?.last_name + 'mentioned you on a folio.');
                     }
                 }
             });
@@ -451,7 +435,7 @@ export class NotificationsService {
 
                         if (owner.integrations.firebase_token) {
                             // Send the notification to firebase for mobile notify
-                            this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a task.');
+                            firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a task.');
                         }
                     }
                 }
@@ -483,7 +467,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a task.');
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Assignment', actor?.first_name + ' ' + actor?.last_name + 'assigned you a task.');
                 }
             }
         } catch (err) {
@@ -515,7 +499,7 @@ export class NotificationsService {
 
                     if (owner.integrations.firebase_token) {
                         // Send the notification to firebase for mobile notify
-                        this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Task Status Changed', actor?.first_name + ' ' + actor?.last_name + ' changed the task ' + task?.title + ' to ' + status);
+                        firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Task Status Changed', actor?.first_name + ' ' + actor?.last_name + ' changed the task ' + task?.title + ' to ' + status);
                     }
                 }
 
@@ -542,7 +526,7 @@ export class NotificationsService {
 
                         if (owner.integrations.firebase_token) {
                             // Send the notification to firebase for mobile notify
-                            this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Task Status Changed', actor?.first_name + ' ' + actor?.last_name + ' changed the task ' + task?.title + ' to ' + status);
+                            firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Task Status Changed', actor?.first_name + ' ' + actor?.last_name + ' changed the task ' + task?.title + ' to ' + status);
                         }
                     }
                     
@@ -576,7 +560,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Comment', actor?.first_name + ' ' + actor?.last_name + 'commented on ');
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Comment', actor?.first_name + ' ' + actor?.last_name + 'commented on ');
                 }
             }
         } catch (err) {
@@ -606,7 +590,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Like on a Post', actor?.first_name + ' ' + actor?.last_name + ' liked your comment on ' + comment._post.title);
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Like on a Post', actor?.first_name + ' ' + actor?.last_name + ' liked your comment on ' + comment._post.title);
                 }
             }
         } catch (err) {
@@ -635,7 +619,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Follow on a Post', actor?.first_name + ' ' + actor?.last_name + ' follows ' + post.title);
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Follow on a Post', actor?.first_name + ' ' + actor?.last_name + ' follows ' + post.title);
                 }
             }
         } catch (err) {
@@ -661,7 +645,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Like on a Post', actor?.first_name + ' ' + actor?.last_name + ' likes ' + post.title);
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Like on a Post', actor?.first_name + ' ' + actor?.last_name + ' likes ' + post.title);
                 }
             }
         } catch (err) {
@@ -698,7 +682,7 @@ export class NotificationsService {
 
                     if (user.integrations.firebase_token) {
                         // Send the notification to firebase for mobile notify
-                        this.sendFirebaseNotification(user._workspace._id || user._workspace, user.integrations.firebase_token, 'Octonius - New Post', actor?.first_name + ' ' + actor?.last_name + ' posted ' + post.title);
+                        firebaseNotifications.sendFirebaseNotification(user._workspace._id || user._workspace, user.integrations.firebase_token, 'Octonius - New Post', actor?.first_name + ' ' + actor?.last_name + ' posted ' + post.title);
                     }
                 }
 
@@ -732,7 +716,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Joined Group', actor?.first_name + ' ' + actor?.last_name + ' added you to group ' + group.group_name);
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Joined Group', actor?.first_name + ' ' + actor?.last_name + ' added you to group ' + group.group_name);
                 }
             }
 
@@ -765,7 +749,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Leave Group', actor?.first_name + ' ' + actor?.last_name + ' removed you from group ' + group.group_name);
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Leave Group', actor?.first_name + ' ' + actor?.last_name + ' removed you from group ' + group.group_name);
                 }
             }
 
@@ -836,7 +820,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Approval Flow Launched', actor?.first_name + ' ' + actor?.last_name + ' launched the approval flow on ' + (itemDB.title || itemDB.original_name));
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - New Approval Flow Launched', actor?.first_name + ' ' + actor?.last_name + ' launched the approval flow on ' + (itemDB.title || itemDB.original_name));
                 }
             }
 
@@ -900,7 +884,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Approval Flow Rejected', actor?.first_name + ' ' + actor?.last_name + ' rejected the approval flow on ' + (itemDB.title || itemDB.original_name));
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Approval Flow Rejected', actor?.first_name + ' ' + actor?.last_name + ' rejected the approval flow on ' + (itemDB.title || itemDB.original_name));
                 }
             }
 
@@ -958,7 +942,7 @@ export class NotificationsService {
 
                 if (owner.integrations.firebase_token) {
                     // Send the notification to firebase for mobile notify
-                    this.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Approval Flow Rejected', 'Approval flow on ' + (itemDB.title || itemDB.original_name) + ' has been approved');
+                    firebaseNotifications.sendFirebaseNotification(owner._workspace._id || owner._workspace, owner.integrations.firebase_token, 'Octonius - Approval Flow Rejected', 'Approval flow on ' + (itemDB.title || itemDB.original_name) + ' has been approved');
                 }
             }
 
@@ -1019,7 +1003,7 @@ export class NotificationsService {
 
                     if (user.integrations.firebase_token) {
                         // Send the notification to firebase for mobile notify
-                        this.sendFirebaseNotification(shuttleGroup._workspace._id || shuttleGroup._workspace, user.integrations.firebase_token, 'Octonius - Shuttle Task', actor?.first_name + ' ' + actor?.last_name + ' assigned a Shuttle Task to your group ' + shuttleGroup.group_name);
+                        firebaseNotifications.sendFirebaseNotification(shuttleGroup._workspace._id || shuttleGroup._workspace, user.integrations.firebase_token, 'Octonius - Shuttle Task', actor?.first_name + ' ' + actor?.last_name + ' assigned a Shuttle Task to your group ' + shuttleGroup.group_name);
                     }
                 }
 
@@ -1103,34 +1087,4 @@ export class NotificationsService {
             throw err;
         }
     }
-
-     async sendFirebaseNotification(workspaceId: string, registrationToken: string, messageTitle: string, messageBody: string) {
-
-        if (registrationToken && messageTitle && messageBody) {         
-
-            var payload = {
-                notification: {
-                    title: messageTitle,
-                    body: messageBody,
-                    data: {
-                        workspaceId: workspaceId
-                    }
-                }
-                //, topic: 'octonius'
-            };
-
-            var options = {
-                priority: "high",
-                timeToLive: 60 * 60 *24
-            };
-
-            admin.messaging().sendToDevice(registrationToken, payload, options)
-                .then((response) => {
-                    console.log("Successfully sent message:", response);
-                })
-                .catch((error) => {
-                    console.log("Error sending message:", error);
-                });
-        }
-    };
 }
