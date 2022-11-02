a4a135711346003ab9796b3c83cb2d95cb1a6a05
Juan Alvarez <juan@octonius.com>
2022-11-02 10:17:46 +0100

    remove script for dev_front

--- stop-dev-front.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Octonius stop development server
-
-echo "Stoping all the node services..."
-
-# Stops and kills the client server from port 4200
-pm2 stop client-server
-pm2 delete client-server
