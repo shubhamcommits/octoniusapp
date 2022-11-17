2ba2bb4aacb14c10c37c945225aaadab3363f5c3
Cosmin Ciobanu <cosmin@octonius.com>
2022-11-17 15:42:16 +0200

    pushing correct file back

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -9,9 +9,9 @@
 
 // DNS Details
 const url = {
-  protocol: `https`, // standard protocol
-  domain: `workplace.octonius.com`, // your domain name where application is supposed to be visible
-  websocket: `wss` // wss in case of https protocol, else pass ws here
+  protocol: `http`, // standard protocol
+  domain: `localhost`, // your domain name where application is supposed to be visible
+  websocket: `ws` // wss in case of https protocol, else pass ws here
 }
 
 export const environment = {
@@ -45,70 +45,70 @@ export const environment = {
   clientUrl: `${url.protocol}://${url.domain}:4200`,
 
   // Octo-doc URL
-  REAL_TIME_URL: `${url["domain"]}/editor`,
+  REAL_TIME_URL: `${url.domain}:3001`,
 
   // Authentication URLs
-  AUTH_BASE_URL: `${url["protocol"]}://${url["domain"]}/auths`,
-  AUTH_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/auths`,
+  AUTH_BASE_URL: `${url.protocol}://${url.domain}:3000`,
+  AUTH_BASE_API_URL: `${url.protocol}://${url.domain}:3000/api`,
 
   // Groups URLs
-  GROUPS_BASE_URL: `${url["protocol"]}://${url["domain"]}/groups`,
-  GROUPS_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/groups`,
+  GROUPS_BASE_URL: `${url.protocol}://${url.domain}:4000`,
+  GROUPS_BASE_API_URL: `${url.protocol}://${url.domain}:4000/api`,
 
   // Workspace URLs
-  WORKSPACE_BASE_URL: `${url["protocol"]}://${url["domain"]}/workspaces`,
-  WORKSPACE_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/workspaces`,
+  WORKSPACE_BASE_URL: `${url.protocol}://${url.domain}:5000`,
+  WORKSPACE_BASE_API_URL: `${url.protocol}://${url.domain}:5000/api`,
 
   // Search Service URLs
-  SEARCH_BASE_URL: `${url["protocol"]}://${url["domain"]}/search`,
-  SEARCH_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/search`,
-
-  // Slack Auth URLs
-  INTEGRATIONS_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/integrations`,
+  SEARCH_BASE_URL: `${url.protocol}://${url.domain}:12000`,
+  SEARCH_BASE_API_URL: `${url.protocol}://${url.domain}:12000/api`,
 
   // User URLs
-  USER_BASE_URL: `${url["protocol"]}://${url["domain"]}/users`,
-  USER_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/users`,
+  USER_BASE_URL: `${url.protocol}://${url.domain}:7000`,
+  USER_BASE_API_URL: `${url.protocol}://${url.domain}:7000/api`,
+
+  // Slack Auth URLs
+  INTEGRATIONS_BASE_API_URL: `${url.protocol}://${url.domain}:13000/api`,
 
   // Posts URLs
-  POST_BASE_URL: `${url["protocol"]}://${url["domain"]}/posts`,
-  POST_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/posts`,
+  POST_BASE_URL: `${url.protocol}://${url.domain}:8000`,
+  POST_BASE_API_URL: `${url.protocol}://${url.domain}:8000/api`,
 
   // Notifications URLs
-  FLAMINGO_BASE_URL: `${url["protocol"]}://${url["domain"]}/flamingo`,
-  FLAMINGO_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/flamingo`,
+  NOTIFICATIONS_BASE_URL: `ws://${url.domain}:9000`,
+  NOTIFICATIONS_BASE_API_URL: `${url.protocol}://${url.domain}:9000/api`,
 
   // Notifications URLs
-  NOTIFICATIONS_BASE_URL: `${url["websocket"]}://${url["domain"]}`,
-  NOTIFICATIONS_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/notifications`,
+  FLAMINGO_BASE_URL: `${url.protocol}://${url.domain}:14000`,
+  FLAMINGO_BASE_API_URL: `${url.protocol}://${url.domain}:14000/api`,
 
   // Approval URLs
-  APPROVAL_BASE_URL: `${url["websocket"]}://${url["domain"]}`,
-  APPROVAL_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/approval`,
+  APPROVAL_BASE_URL: `${url.protocol}://${url.domain}:15000`,
+  APPROVAL_BASE_API_URL: `${url.protocol}://${url.domain}:15000/api`,
 
   // Utilities URLs
-  UTILITIES_BASE_URL: `${url["protocol"]}://${url["domain"]}/utilities`,
-  UTILITIES_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/utilities`,
-  UTILITIES_GROUPS_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/groups`,
-  UTILITIES_FILES_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/files`,
-  //UTILITIES_GROUP_FILES_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/groupsFiles`,
-  UTILITIES_POSTS_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/posts`,
-  UTILITIES_USERS_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/users`,
-  UTILITIES_WORKSPACES_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/workspaces`,
-  UTILITIES_FLAMINGOS_UPLOADS: `${url["protocol"]}://${url["domain"]}/uploads/flamingo`,
+  UTILITIES_BASE_URL: `${url.protocol}://${url.domain}:10000`,
+  UTILITIES_BASE_API_URL: `${url.protocol}://${url.domain}:10000/api`,
+  UTILITIES_GROUPS_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/groups`,
+  //UTILITIES_GROUP_FILES_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/groupsFiles`,
+  UTILITIES_FILES_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/files`,
+  UTILITIES_POSTS_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/posts`,
+  UTILITIES_USERS_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/users`,
+  UTILITIES_WORKSPACES_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/workspaces`,
+  UTILITIES_FLAMINGOS_UPLOADS: `${url.protocol}://${url.domain}:10000/uploads/flamingo`,
 
-  //LIBREOFFICE_BASE_URL
-  LIBREOFFICE_BASE_URL:  `${url["protocol"]}://${url["domain"]}/libreoffice`,
+  LIBREOFFICE_BASE_URL: `${url.protocol}://${url.domain}:9980/`,
+  LIBREOFFICE_BASE_PORT: '9980',
 
   // Folio URLs
-  FOLIO_BASE_URL: `${url["websocket"]}://${url["domain"]}/folio`,
-  FOLIO_HTTP_URL: `${url["protocol"]}://${url["domain"]}/folio`,
+  FOLIO_BASE_URL: `${url.websocket}://${url.domain}:11000`,
+  FOLIO_HTTP_URL: `${url.protocol}://${url.domain}:11000`,
 
   // Chats URLs
-  CHATS_BASE_API_URL: `${url["protocol"]}://${url["domain"]}/api/chats`,
+  CHATS_BASE_API_URL: `${url.protocol}://${url.domain}:16001/api`,
 
-  // MANAGEMENT_URL
-  MANAGEMENT_URL: `${url["protocol"]}://${url["domain"]}`,
+  // MANAGEMENT_URL: 'https://management.octonius.com',
+  MANAGEMENT_URL: `${url.protocol}://${url.domain}:3300`,
 };
 
 /*
