4f5901b63a509738378e22fabf0f3521e67b6b0e
Juan Alvarez <juan@octonius.com>
2023-01-03 14:29:38 +0100

    Add the option to delete a portfolio

--- client/modules/groups/portfolio/portfolio-header/portfolio-header.component.ts
+++ client/modules/groups/portfolio/portfolio-header/portfolio-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Injector, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, Injector, Input } from '@angular/core';
 import { PublicFunctions } from 'modules/public.functions';
 import { UtilityService } from 'src/shared/services/utility-service/utility.service';
 import { environment } from 'src/environments/environment';
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { PortfolioService } from 'src/shared/services/portfolio-service/portfolio.service';
 import { ColorPickerDialogComponent } from 'src/app/common/shared/color-picker-dialog/color-picker-dialog.component';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio-header',
@@ -53,7 +54,7 @@ export class PortfolioHeaderComponent implements OnInit {
   constructor(
     public injector: Injector,
     public dialog: MatDialog,
-    private changeDetection: ChangeDetectorRef,
+    private router: Router,
     private utilityService: UtilityService,
     private portfolioService: PortfolioService
   ) { }
@@ -210,6 +211,32 @@ export class PortfolioHeaderComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       colorPickedSubs.unsubscribe();
     });
+  }
+
+  deletePortfolio() {
+    this.utilityService.getConfirmDialogAlert()
+      .then((result) => {
+        if (result.value) {
+          // Remove the file
+          this.utilityService.asyncNotification($localize`:@@portfolioHeader.pleaseWaitDeleting:Please wait we are deleting the portfolio...`, new Promise((resolve, reject) => {
+            this.portfolioService.deletePortfolio(this.portfolioData?._id)
+              .then((res) => {
+                this.publicFunctions.sendUpdatesToPortfolioData({});
+                this.router.navigate(['/dashboard', 'work', 'groups', 'all'],
+                    {
+                      queryParams: {
+                        selectedTab: 'portfolio'
+                      }
+                    }
+                  );
+
+                resolve(this.utilityService.resolveAsyncPromise($localize`:@@portfolioHeader.deleted:Portfolio deleted!`));
+              }).catch((err) => {
+                reject(this.utilityService.rejectAsyncPromise($localize`:@@portfolioHeader.unableDelete:Unable to delete the portfolio, please try again!`));
+              });
+          }));
+        }
+      });
   }
 
   objectExists(object: any) {
