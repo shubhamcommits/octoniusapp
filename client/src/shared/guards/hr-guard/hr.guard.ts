0572887f8b824f3480f3788625ffa3d35c0cdce7
Juan Alvarez <juan@octonius.com>
2023-01-05 12:24:55 +0100

    update HR guard

--- client/src/shared/guards/hr-guard/hr.guard.ts
+++ client/src/shared/guards/hr-guard/hr.guard.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { CanActivate, CanActivateChild, CanLoad, Router, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { PublicFunctions } from 'modules/public.functions';
 import { Observable } from 'rxjs/internal/Observable';
 import { StorageService } from 'src/shared/services/storage-service/storage.service';
 import { UtilityService } from 'src/shared/services/utility-service/utility.service';
@@ -9,30 +10,34 @@ import { UtilityService } from 'src/shared/services/utility-service/utility.serv
 })
 export class HRGuard implements CanActivate, CanActivateChild, CanLoad {
 
+  private publicFunctions = new PublicFunctions(this.injector);
+
   constructor(
-    private storageService: StorageService,
+    private injector: Injector,
     private utilityService: UtilityService,
     private router: Router
   ){ }
 
-  canActivate(
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const userData: any = this.storageService.getLocalData('userData');
+    state: RouterStateSnapshot) {
+      const userData: any = await this.publicFunctions.getCurrentUser();
       const role = (userData?.role) ? userData?.role?.trim() : '';
-      if (userData.hr_role || role == 'owner') {
+      if (userData?.hr_role || role == 'owner') {
         return true;
       } else {
-        this.utilityService.warningNotification($localize`:@@hrGuard.oopsNoPermission:Oops seems like you don\'t have the permission to access the admin section, kindly contact your superior to provide you the proper admin rights!`);
+        this.utilityService.warningNotification($localize`:@@hrGuard.oopsNoPermission:Oops seems like you don\'t have the permission to access the HR section, kindly contact your superior to provide you the proper admin rights!`);
         this.router.navigate(['dashboard', 'myspace', 'inbox']);
         return false;
       }
   }
+
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
