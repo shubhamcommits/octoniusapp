5448adcf54ec4c9b815784d55fe88b4bb885d58b
Juan Alvarez <juan@octonius.com>
2023-03-07 19:28:43 +0100

    Add translations

--- client/src/shared/guards/library-guard/library.guard.ts
+++ client/src/shared/guards/library-guard/library.guard.ts
@@ -89,7 +89,6 @@ export class LibraryGuard implements CanActivateChild  {
     }
 
     const userCanViewCollection = await this.utilityService.canUserDoCollectionAction(collectionData, currentGroup, userData, 'read');
-console.log(userCanViewCollection);
     // if (userCanViewCollection && userData?._private_group != currentGroup?._id) {
     if (userCanViewCollection) {
       return true;
