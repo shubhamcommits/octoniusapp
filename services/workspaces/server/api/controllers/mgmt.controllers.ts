d08d62d37b8ca267530c574be56cdba096c0ab23
Juan Alvarez <juan@octonius.com>
2023-07-20 10:23:43 +0200

    Add log for getting subscription

--- services/workspaces/server/api/controllers/mgmt.controllers.ts
+++ services/workspaces/server/api/controllers/mgmt.controllers.ts
@@ -448,7 +448,7 @@ export class ManagementControllers {
             await managementService.getSubscription(user._workspace._id, user._workspace.management_private_api_key)
                 .then(res => {
 console.log(res);
-                    subscription = res['subscription'];
+                    subscription = res.data.subscription;
                 });
 
             // const workspace = await Workspace.findById({ _id: workspaceId })
