7644f5391b47fc8dc2985ac6603b3ba512e09dc4
Juan Alvarez <juan@octonius.com>
2023-12-07 11:32:15 +0100

    test open google sync

--- client/modules/admin/admin-general/workplace-integrations/workplace-google-sync/workplace-google-sync.component.ts
+++ client/modules/admin/admin-general/workplace-integrations/workplace-google-sync/workplace-google-sync.component.ts
@@ -49,12 +49,15 @@ export class WorkplaceGoogleSyncComponent implements OnInit {
     }
 
     this.googleTokenClient.callback = async (resp) => {
+console.log(resp);
       if (resp.error !== undefined) {
         throw (resp);
       }
 
       const googleUser: any = await this.getGoogleLoggedInUser();
+console.log(googleUser);
       const schemas: any = await this.getUserSchema(googleUser.customerId);
+console.log(schemas);
       this.utilityService.updateIsLoadingSpinnerSource(false);
 
       if (schemas) {
