10125be9be3cdbff750e06c35d9cb78046de403c
juan@octonius.com <juan@octonius.com>
2025-03-10 17:06:48 +0100

    update yarn & fix app.ts error

--- services/workspaces/server/api/app.ts
+++ services/workspaces/server/api/app.ts
@@ -1,46 +1,66 @@
-import express, { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import compression from 'compression';
-import { developmentConfig, productionConfig } from '../configs';
-import { domainRoutes, loungesRoutes, memberRoutes, mgmtRoutes, workspaceRoutes, storiesRoutes, hrRoutes, crmRoutes } from './routes';
-import fileUpload from 'express-fileupload';
-import { fileHandler } from '../utils';
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import cors from "cors";
+import morgan from "morgan";
+import compression, { CompressionOptions } from "compression";
+import { developmentConfig, productionConfig } from "../configs";
+import {
+    domainRoutes,
+    loungesRoutes,
+    memberRoutes,
+    mgmtRoutes,
+    workspaceRoutes,
+    storiesRoutes,
+    hrRoutes,
+    crmRoutes,
+} from "./routes";
+import fileUpload from "express-fileupload";
+import { fileHandler } from "../utils";
 
 // Defining new Express application
 const app = express();
 
 // Load configuration based on the environment states
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
     developmentConfig();
 } else {
     productionConfig();
 }
 
 // Initiliazing Database Connection
-require('../db');
+require("../db");
 
-// cors middleware for orign and Headers
+// cors middleware for origin and Headers
 app.use(cors());
 
-// Adding The 'body-parser' middleware only handles JSON and urlencoded data
-app.use(express.json({ limit:'60mb' }))
+// Compressing the Application
+app.use(compression() as unknown as express.RequestHandler);
 
-// body parsers
-app.use(bodyParser.json({ limit:'60mb' }));
-app.use(bodyParser.urlencoded({ limit: '60mb', parameterLimit: 100000, extended: true }));
+// Adding The 'express.json' middleware only handles JSON and urlencoded data
+app.use(express.json({ limit: "60mb" }));
+app.use(
+    express.urlencoded({
+        limit: "60mb",
+        parameterLimit: 100000,
+        extended: true,
+    })
+);
 
 // Use Morgan middleware for logging every request status on console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Allow any method from any host and log requests
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-    if ('OPTIONS' === req.method) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, DELETE"
+    );
+    if ("OPTIONS" === req.method) {
         res.sendStatus(200);
     } else {
         console.log(`${req.ip} ${req.method} ${req.url}`);
@@ -48,56 +68,51 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     }
 });
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// Handle POST requests that come in formatted as JSON
-app.use(express.json({ limit:'60mb' }));
-
 // Handling GZIPPED ROUTES
 const encodeResToGzip = (contentType: any) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        req.url = req.url + '.gz';
-        res.set('Content-Encoding', 'gzip');
-        res.set('Content-Type', contentType);
+        req.url = req.url + ".gz";
+        res.set("Content-Encoding", "gzip");
+        res.set("Content-Type", contentType);
         next();
     };
 };
 
 // Converting static files to Gzipped Extensions
-app.get("*.js", encodeResToGzip('text/javascript'));
-app.get("*.css", encodeResToGzip('text/css'));
+app.get("*.js", encodeResToGzip("text/javascript"));
+app.get("*.css", encodeResToGzip("text/css"));
 
 // Set file upload middleware
-app.use(fileUpload({
-    limits: {
-        fileSize: 1024 * 1024 * 1024 * 1024
-    },
-    abortOnLimit: true
-}));
+app.use(
+    fileUpload({
+        limits: {
+            fileSize: 1024 * 1024 * 1024 * 1024,
+        },
+        abortOnLimit: true,
+    }) as unknown as express.RequestHandler
+);
 
 // Availing the static uploads folder to access from server
-// app.use('/uploads', express.static(process.env.FILE_UPLOAD_FOLDER));
-app.use('/uploads', fileHandler);
+app.use("/uploads", fileHandler);
 
 // Routes which should handle request
-app.all('/', (req: Request, res: Response, next: NextFunction) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+app.get("/", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 // Correct REST naming
-app.use('/api/domains', domainRoutes);
-app.use('/api/members', memberRoutes);
-app.use('/api/management', mgmtRoutes);
-app.use('/api/lounges', loungesRoutes);
-app.use('/api/stories', storiesRoutes);
-app.use('/api/hr', hrRoutes);
-app.use('/api/crm', crmRoutes);
-app.use('/api', workspaceRoutes);
+app.use("/api/domains", domainRoutes);
+app.use("/api/members", memberRoutes);
+app.use("/api/management", mgmtRoutes);
+app.use("/api/lounges", loungesRoutes);
+app.use("/api/stories", storiesRoutes);
+app.use("/api/hr", hrRoutes);
+app.use("/api/crm", crmRoutes);
+app.use("/api", workspaceRoutes);
 
 // Invalid routes handling middleware
 app.use((req: Request, res: Response, next: NextFunction) => {
-    const error = new Error('404 not found');
+    const error = new Error("404 not found");
     next(error);
 });
 
@@ -106,12 +121,9 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     res.status(error.status || 500);
     res.json({
         error: {
-            message: error.message
-        }
+            message: error.message,
+        },
     });
 });
 
-// Compressing the Application
-app.use(compression());
-
-export { app }
+export { app };
