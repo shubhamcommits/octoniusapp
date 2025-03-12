408b991ba1b78eda62ea6618c14a8a0282b1896d
juan@octonius.com <juan@octonius.com>
2025-03-12 12:13:26 +0100

    only admin or owner can transfer ownership

--- client/modules/admin/admin-general/admin-general.component.ts
+++ client/modules/admin/admin-general/admin-general.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit, Injector, AfterContentChecked, OnDestroy } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { PublicFunctions } from 'modules/public.functions';
-import { AuthService } from 'src/shared/services/auth-service/auth.service';
-import { StorageService } from 'src/shared/services/storage-service/storage.service';
-import { UtilityService } from 'src/shared/services/utility-service/utility.service';
-import { WorkspaceService } from 'src/shared/services/workspace-service/workspace.service';
-import { SubSink } from 'subsink';
-import { WorkspaceRolesInformationDialogComponent } from './workspace-roles-information-dialog/workspace-roles-information-dialog.component';
-import { ManagementPortalService } from 'src/shared/services/management-portal-service/management-portal.service';
+import {
+  Component,
+  OnInit,
+  Injector,
+  AfterContentChecked,
+  OnDestroy,
+} from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { PublicFunctions } from "modules/public.functions";
+import { AuthService } from "src/shared/services/auth-service/auth.service";
+import { StorageService } from "src/shared/services/storage-service/storage.service";
+import { UtilityService } from "src/shared/services/utility-service/utility.service";
+import { WorkspaceService } from "src/shared/services/workspace-service/workspace.service";
+import { SubSink } from "subsink";
+import { WorkspaceRolesInformationDialogComponent } from "./workspace-roles-information-dialog/workspace-roles-information-dialog.component";
+import { ManagementPortalService } from "src/shared/services/management-portal-service/management-portal.service";
 
 @Component({
-  selector: 'app-admin-general',
-  templateUrl: './admin-general.component.html',
-  styleUrls: ['./admin-general.component.scss']
+  selector: "app-admin-general",
+  templateUrl: "./admin-general.component.html",
+  styleUrls: ["./admin-general.component.scss"],
 })
-export class AdminGeneralComponent implements OnInit, AfterContentChecked, OnDestroy {
+export class AdminGeneralComponent
+  implements OnInit, AfterContentChecked, OnDestroy
+{
+  workspaceData: any;
 
-  workspaceData: Object;
-
-  userData: Object;
+  userData: any;
 
   allowDecentralizedRoles = false;
 
@@ -41,27 +48,30 @@ export class AdminGeneralComponent implements OnInit, AfterContentChecked, OnDes
     private router: Router,
     private injector: Injector,
     public dialog: MatDialog
-  ) { }
+  ) {}
 
   async ngOnInit() {
-
     // Setting Home State
     this.publicFunctions.sendUpdatesToRouterState({
-      state: 'admin'
-    })
+      state: "admin",
+    });
 
     //this.utilityService.startForegroundLoader();
     this.userData = await this.publicFunctions.getCurrentUser();
     this.workspaceData = await this.publicFunctions.getCurrentWorkspace();
-    this.allowDecentralizedRoles = this.workspaceData['allow_decentralized_roles'];
+    this.allowDecentralizedRoles =
+      this.workspaceData["allow_decentralized_roles"];
 
-    this.isIndividualSubscription = await this.publicFunctions.checkIsIndividualSubscription();
+    this.isIndividualSubscription =
+      await this.publicFunctions.checkIsIndividualSubscription();
   }
 
   ngAfterContentChecked() {
-    this.subSink.add(this.utilityService.isLoadingSpinner.subscribe((res) => {
-      this.isLoading$ = res;
-    }));
+    this.subSink.add(
+      this.utilityService.isLoadingSpinner.subscribe((res) => {
+        this.isLoading$ = res;
+      })
+    );
   }
 
   ngOnDestroy() {
@@ -74,7 +84,7 @@ export class AdminGeneralComponent implements OnInit, AfterContentChecked, OnDes
     this.storageService.clear();
     this.publicFunctions.sendUpdatesToRouterState({});
     this.publicFunctions.sendUpdatesToUserData({});
-    this.router.navigate(['/home']);
+    this.router.navigate(["/home"]);
   }
 
   onWorkspaceUpdated(workspace: any) {
@@ -84,22 +94,37 @@ export class AdminGeneralComponent implements OnInit, AfterContentChecked, OnDes
 
   saveSettings(selected) {
     // Save the settings
-    this.utilityService.asyncNotification($localize`:@@adminGeneral.pleaseWaitsavingSettings:Please wait, we are saving the new setting...`,
-    new Promise((resolve, reject)=>{
-        this.workspaceService.saveSettings(this.workspaceData['_id'], {allow_decentralized_roles: selected.checked})
-          .then(()=> {
-            this.workspaceData['allow_decentralized_roles'] = selected.checked;
-            this.publicFunctions.sendUpdatesToWorkspaceData(this.workspaceData);
-            resolve(this.utilityService.resolveAsyncPromise($localize`:@@adminGeneral.settingsSaved:Settings saved!`));
+    this.utilityService.asyncNotification(
+      $localize`:@@adminGeneral.pleaseWaitsavingSettings:Please wait, we are saving the new setting...`,
+      new Promise((resolve, reject) => {
+        this.workspaceService
+          .saveSettings(this.workspaceData["_id"], {
+            allow_decentralized_roles: selected.checked,
           })
-          .catch(() => reject(this.utilityService.rejectAsyncPromise($localize`:@@adminGeneral.unableToSaveGroupSettings:Unable to save the settings, please try again!`)));
-    }));
+          .then(() => {
+            this.workspaceData["allow_decentralized_roles"] = selected.checked;
+            this.publicFunctions.sendUpdatesToWorkspaceData(this.workspaceData);
+            resolve(
+              this.utilityService.resolveAsyncPromise(
+                $localize`:@@adminGeneral.settingsSaved:Settings saved!`
+              )
+            );
+          })
+          .catch(() =>
+            reject(
+              this.utilityService.rejectAsyncPromise(
+                $localize`:@@adminGeneral.unableToSaveGroupSettings:Unable to save the settings, please try again!`
+              )
+            )
+          );
+      })
+    );
   }
 
   openDialog() {
     this.dialog.open(WorkspaceRolesInformationDialogComponent, {
       disableClose: false,
-      hasBackdrop: true
+      hasBackdrop: true,
     });
   }
 }
