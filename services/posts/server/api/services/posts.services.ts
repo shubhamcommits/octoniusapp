cd5c36a81bfda7331bc34539e05de7b5aa3e7cfe
juan@octonius.com <juan@octonius.com>
2025-03-23 12:06:46 +0100

    remove console.logs

--- services/posts/server/api/services/posts.services.ts
+++ services/posts/server/api/services/posts.services.ts
@@ -2532,15 +2532,11 @@ export class PostService {
                 .populate({ path: "_assigned_to", select: this.userFields })
                 .lean();
 
-            posts = posts.filter((tmpPost) => {
-                console.log(tmpPost);
-                console.log(tmpPost._posted_by);
-                console.log(userData);
-                return (
+            posts = posts.filter(
+                (tmpPost) =>
                     tmpPost._posted_by._workspace.toString() ==
                     userData._workspace.toString()
-                );
-            });
+            );
 
             for (let i = 0; i < posts.length; i++) {
                 let post = posts[i];
